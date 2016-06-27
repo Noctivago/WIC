bef961bdbf2b519fc8aef2e3da29de2498df60c1
prcunha <prcunha.383@gmail.com>
2016-06-28 00:26:16 +0100

    AJAX TEST

--- func/f_1000.php
+++ func/f_1000.php
@@ -4,12 +4,12 @@ include_once '../db/conn.inc.php';
 
 try {
     $user = $_POST['username'];
-    #$pass = $_POST['password'];
-    #$mail = $_POST['email'];
-    $stmt = $pdo->prepare("INSERT INTO [dbo].[User] ([Username], [Password], [Email]) VALUES (:u, 'p', 'm')");
+    $pass = $_POST['password'];
+    $mail = $_POST['email'];
+    $stmt = $pdo->prepare("INSERT INTO [dbo].[User] ([Username], [Password], [Email]) VALUES (:u, :p, :m)");
     $stmt->bindParam(':u', $user);
-    #$stmt->bindParam(':p', $pass);
-    #$stmt->bindParam(':m', $mail);
+    $stmt->bindParam(':p', $pass);
+    $stmt->bindParam(':m', $mail);
     $stmt->execute();
     //retorna 1 para no sucesso do ajax saber que foi com inserido sucesso
     echo "USER " . $user . " ADDED!";
