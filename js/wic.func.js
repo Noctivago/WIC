ac4e4b60a886d094baf0193eb1f3770ddc90b705
prcunha <prcunha.383@gmail.com>
2016-06-28 00:34:56 +0100

    AJAX TEST

--- js/wic.func.js
+++ js/wic.func.js
@@ -2,7 +2,7 @@ function f_1000() {
     var f_1000_username = document.getElementById("username").value;
     var f_1000_password = document.getElementById("password").value;
     var f_1000_email = document.getElementById("email").value;
-    alert(f_1000_username);
+    //alert(f_1000_username);
     var xhr;
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...  
         xhr = new XMLHttpRequest();
@@ -13,16 +13,16 @@ function f_1000() {
     var d_pass = "password=" + f_1000_password;
     var d_mail = "email=" + f_1000_email;
     xhr.open("POST", "../func/f_1000.php", true);
-    alert('FILE');
+    //alert('FILE');
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     //xhr.send(data);
     xhr.send(d_user, d_pass, d_mail);
     xhr.onreadystatechange = display_data;
     function display_data() {
         if (xhr.readyState === 4) {
-            alert('xhr.readyState === 4');
+            //alert('xhr.readyState === 4');
             if (xhr.status === 200) {
-                alert(xhr.responseText);
+                //alert(xhr.responseText);
                 //document.getElementById("suggestion").innerHTML = xhr.responseText;
                 var ajaxDisplay = document.getElementById('ajaxDiv');
                 ajaxDisplay.innerHTML = xhr.responseText;
