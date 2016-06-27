42625496896315afcb41fee7761d41525ae307de
prcunha <prcunha.383@gmail.com>
2016-06-28 00:47:42 +0100

    AJAX TEST

--- db/conn.inc.php
+++ db/conn.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "wicsqlserver.database.windows.net";
-$port = "1433";
-$pass = '#$youcandoit2017$#';
-$user = "wic";
+$dbserver = "wicsqlserver.database.windows.net";
+$dbport = "1433";
+$dbpass = '#$youcandoit2017$#';
+$dbuser = "wic";
 $db = "wicdb";
 //$server = "wicsqlserver.database.windows.net";
 //$port = "1433";
@@ -12,7 +12,7 @@ $db = "wicdb";
 //$db = "wicdb";
 
 try {
-    $pdo = new PDO("sqlsrv:server=$server; Database=$db", "$user", "$pass");
+    $pdo = new PDO("sqlsrv:server=$dbserver; Database=$db", "$dbuser", "$dbpass");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     if ($pdo == false) {
         echo "Redirect to 404!";
