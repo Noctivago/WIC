884d24fadec2363713dd5ff3a4082eef21c99ba8
prcunha <prcunha.383@gmail.com>
2016-06-28 19:46:44 +0100

    AJAX TEST

--- ajaxsubmit.php
+++ ajaxsubmit.php
@@ -5,23 +5,29 @@ include_once './db/conn.inc.php';
 $arg = (filter_var($_POST['arg'], FILTER_SANITIZE_STRING));
 
 if ($arg === 'addUser') {
-    //addUser
+//addUser
     try {
         $username = (filter_var($_POST['username'], FILTER_SANITIZE_STRING));
         $password = (filter_var($_POST['password'], FILTER_SANITIZE_STRING));
         $email = (filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
         $stmt = $pdo->prepare("INSERT INTO [dbo].[User] ([Username], [Password], [Email]) VALUES (:u, :p, :m)");
         $stmt->bindParam(':u', $username);
-        #$stmt->bindParam(':p', $utilizador->getPassword());
+#$stmt->bindParam(':p', $utilizador->getPassword());
         $stmt->bindParam(':p', $password);
         $stmt->bindParam(':m', $email);
         $stmt->execute();
-        //retorna 1 para no sucesso do ajax saber que foi com inserido sucesso
+//retorna 1 para no sucesso do ajax saber que foi com inserido sucesso
         echo "USER " . $username . " ADDED! w/Password " . $password;
     } catch (Exception $ex) {
-        //retorna 0 para no sucesso do ajax saber que foi um erro
+//retorna 0 para no sucesso do ajax saber que foi um erro
         echo "ERROR!";
     }
+} else if ($arg === 'readUser') {
+    
+} else if ($arg === 'updateUser') {
+    
+} else if ($arg === 'deleteUser') {
+    
 } else {
     echo "IF -> ELSE -> ERROR!";
 }
