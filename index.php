d1de78a54cbcfdb31f0e07dd4257dc372a9a4b62
prcunha <prcunha.383@gmail.com>
2016-06-28 18:42:47 +0100

    AJAX TEST

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 require_once './config/conn.inc.php';
 require_once './poo/User.php';
 if ($_REQUEST["username"] || $_REQUEST["password"] || $_REQUEST["email"]) {
+    $username = ($user = filter_var($_POST['username'], FILTER_SANITIZE_STRING));
+    $password = (filter_var($_POST['password'], FILTER_SANITIZE_STRING));
+    $email = (filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
     $user = new User();
     $user->setUsername($username);
     $user->setPassword($password);
