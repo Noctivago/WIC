0304f12e7e3fe1e53804338c81012f3d84f868bf
prcunha <prcunha.383@gmail.com>
2016-06-30 00:59:15 +0100

    FUNC TUDO OKI!

--- pages/logout.php
+++ pages/logout.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 unset($_SESSION["Username"]);
 //unset($_SESSION["password"]);
@@ -7,3 +6,79 @@ unset($_SESSION["Id"]);
 
 echo 'You have cleaned session';
 header('Refresh: 2; URL = login.php');
+?>
+
+<html lang = "en">
+
+    <head>
+        <title>WIC</title>
+        <link href = "../assets/bootstrap/css/bootstrap.min.css" rel = "stylesheet">
+        <style>
+            body {
+                padding-top: 40px;
+                padding-bottom: 40px;
+                background-color: #ADABAB;
+            }
+
+            .form-signin {
+                max-width: 330px;
+                padding: 15px;
+                margin: 0 auto;
+                color: #017572;
+            }
+
+            .form-signin .form-signin-heading,
+            .form-signin .checkbox {
+                margin-bottom: 10px;
+            }
+
+            .form-signin .checkbox {
+                font-weight: normal;
+            }
+
+            .form-signin .form-control {
+                position: relative;
+                height: auto;
+                -webkit-box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
+                padding: 10px;
+                font-size: 16px;
+            }
+
+            .form-signin .form-control:focus {
+                z-index: 2;
+            }
+
+            .form-signin input[type="email"] {
+                margin-bottom: -1px;
+                border-bottom-right-radius: 0;
+                border-bottom-left-radius: 0;
+                border-color:#017572;
+            }
+
+            .form-signin input[type="password"] {
+                margin-bottom: 10px;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                border-color:#017572;
+            }
+
+            h2{
+                text-align: center;
+                color: #017572;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>LOGOUT</h2> 
+        <div class = "container form-signin">
+        </div> <!-- /container -->
+        <div class = "container">
+            <h1>Hope to see you again!</h1>
+            <!--Click here to clean <a href = "logout.php" tite = "Logout">Session-->
+
+        </div> 
+
+    </body>
+</html>
