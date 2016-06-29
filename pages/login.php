edc8eb9654a4264ef9756600e6d172e502d7192f
prcunha <prcunha.383@gmail.com>
2016-06-30 00:38:40 +0100

    FUNC TUDO OKI!

--- pages/login.php
+++ pages/login.php
@@ -98,9 +98,7 @@ session_start();
 
         <div class = "container">
 
-            <form class = "form-signin" role = "form" 
-                  action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
-            ?>" method = "post">
+            <form class = "form-signin" role = "form" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "post">
                 <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
                 <input type = "text" class = "form-control" 
                        name = "username" placeholder = "username" 
