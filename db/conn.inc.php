80bb9ef6f332a29df262063e2f0079a78e92a1a4
prcunha <prcunha.383@gmail.com>
2016-06-29 19:53:25 +0100

    Merge origin/gh-pages into gh-pages
    Conflicts:
    	ajaxsubmit.php
    	model/User.php

--- db/conn.inc.php
+++ db/conn.inc.php
@@ -21,4 +21,24 @@ try {
     }
 } catch (Exception $e) {
     echo("Error -> IP NOT ALLOWED!");
-}  
+}
+
+// Simple function to handle PDO prepared statements
+function sql($db, $q, $params, $return) {
+
+    try {
+
+        // Prepare statement
+        $stmt = $db->prepare($q);
+        // Execute statement
+        $stmt->execute($params);
+        // Decide whether to return the rows themselves, or just count the rows
+        if ($return == "rows") {
+            return $stmt->fetchAll();
+        } elseif ($return == "count") {
+            return $stmt->rowCount();
+        }
+    } catch (Exception $exc) {
+        echo $exc->getTraceAsString();
+    }
+}
