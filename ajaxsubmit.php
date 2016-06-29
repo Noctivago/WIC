27b62652e023f949945958582888bacf004fe5dc
prcunha <prcunha.383@gmail.com>
2016-06-29 23:00:37 +0100

    FUNC TUDO OKI!

--- ajaxsubmit.php
+++ ajaxsubmit.php
@@ -8,13 +8,13 @@ include_once './db/conn.inc.php';
 //sql($db, "UPDATE table SET name = ? WHERE id = ?", array($name, $id));
 
 
-$arg = (filter_var(INPUT_POST, ['arg'], FILTER_SANITIZE_STRING));
+$arg = (filter_var(INPUT_POST, 'arg', FILTER_SANITIZE_STRING));
 
 if ($arg === 'addUser') {
     try {
-        $username = (filter_var(INPUT_POST, ['username'], FILTER_SANITIZE_STRING));
-        $password = (filter_var(INPUT_POST, ['password'], FILTER_SANITIZE_STRING));
-        $email = (filter_var(INPUT_POST, ['email'], FILTER_SANITIZE_EMAIL));
+        $username = (filter_var(INPUT_POST, 'username', FILTER_SANITIZE_STRING));
+        $password = (filter_var(INPUT_POST, 'password', FILTER_SANITIZE_STRING));
+        $email = (filter_var(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
         sql($pdo, "INSERT INTO [dbo].[User] ([Username], [Password], [Email]) VALUES (?, ?, ?)", array($username, $password, $email));
         echo "USER " . $username . " ADDED! w/Password " . $password;
     } catch (Exception $ex) {
@@ -54,7 +54,7 @@ if ($arg === 'addUser') {
 } else if ($arg === 'blockUser') {
     
 } else if ($arg === 'addNews') {
-    $UserEmail = (filter_var(INPUT_POST, ['email'], FILTER_SANITIZE_STRING));
+    $UserEmail = (filter_var(INPUT_POST, 'email', FILTER_SANITIZE_STRING));
     $count = sql($pdo, "SELECT * FROM [dbo].[News] WHERE [Email] = ? ", array($UserEmail), "count");
     //IF EXISTS -1
     if ($count < 0) {
