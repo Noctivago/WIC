337cafb4842868e9933029af62c627b03be96161
prcunha <prcunha.383@gmail.com>
2016-06-30 00:44:53 +0100

    FUNC TUDO OKI!

--- pages/session.php
+++ pages/session.php
@@ -7,7 +7,7 @@ $userId = $_SESSION['Id'];
 $userUsername = $_SESSION['Username'];
 //Query To Fetch Complete Information Of User
 $user = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? and [Id] = ?", array($userUsername, $userId), "rows");
-$login_session = $row['Username'];
+$login_session = $user['Username'];
 if (!isset($login_session)) {
     $pdo = null;
     header('Location: login.php'); // Redirecting To Home Page
