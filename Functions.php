5ce412547eb6b6230e57f2fb576e81f7d883ad0b
Jesualdo Teixeira da Cunha <jesualdocunha@gmail.com>
2016-06-29 10:10:59 +0100

    teste

---
+++ Functions.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once './db/conn.inc.php';
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function Login($username,$password){
+    $sql ="SELECT [Id]
+      ,[Username]
+      ,[Password]
+      ,[Email]
+      ,[Account_Enabled]
+      ,[Date_Created]
+      ,[Login_Failed]
+      ,[Last_Login]
+      ,[Abusive_User]
+      ,[Good_User]
+      ,[Status]
+      ,[Last_Status_online]
+       FROM [dbo].[User]
+       where [dbo].[User].[Username]='$username'";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetch();
+    return $result;
+}
+
+
+
+?>
