d1a930767d8064ef96d10569cb183de0556926df
prcunha <prcunha.383@gmail.com>
2016-07-01 00:09:22 +0100

    LOGIN -> LOGOUT -> SESSION & PROFILE -> GENERATE ACTIVATION CODE -> ACTIVATING ACCOUNT

--- pages/signup.php
+++ pages/signup.php
@@ -89,7 +89,7 @@ include_once '../db/functions.php';
                     try {
                         //GERA CODIGO DE ATIVACAO DE 128car
                         $code = generateActivationCode($length = 64);
-                        sql($pdo, "INSERT INTO [dbo].[User] ([Username], [Password], [Email], [Account_Enabled], [User_Code_Activation], [Login_Failed]) VALUES (?, ?, ?, ?, ?)", array($username, $hashPassword, $email, '0', $code, '0'));
+                        sql($pdo, "INSERT INTO [dbo].[User] ([Username], [Password], [Email], [Account_Enabled], [User_Code_Activation], [Login_Failed]) VALUES (?, ?, ?, ?, ?, ?)", array($username, $hashPassword, $email, '0', $code, '0'));
                         $msg = 'USER ' . $username . ' ADDED!';
                         header('Location: account-confirmation.php');
                     } catch (Exception $ex) {
