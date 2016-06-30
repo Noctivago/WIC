ecd885aec05aa28e4971624e8745a30fdb18ea31
prcunha <prcunha.383@gmail.com>
2016-06-30 01:42:06 +0100

    FUNC TUDO OKI!

--- pages/session.php
+++ pages/session.php
@@ -6,6 +6,10 @@ session_start(); // Starting Session
 if (!isset($_SESSION['username'])) {
     header('Location: login.php'); // Redirecting To Home Page
 }
+
+
+#SE ESTIVER ATRIBUIDO VERIFICAR NOVAMENTE SE EXISTE NA BD <- ADICIONAR PASSWORD A SESSION
+
 //} else {
 //    //ADICIONAR PASSWORD
 //    $rows = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? AND [Id] = ?", array($_SESSION['username'], $_SESSION['id']), "rows");
