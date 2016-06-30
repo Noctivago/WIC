6dacfb5eccd35136161f41e6d205213daefd3a5f
prcunha <prcunha.383@gmail.com>
2016-06-30 01:39:35 +0100

    FUNC TUDO OKI!

--- pages/profile.php
+++ pages/profile.php
@@ -1,6 +1,6 @@
 <?php
 //
-include('session.php');
+include_once ('session.php');
 #session_start();
 ?>
 
--- pages/session.php
+++ pages/session.php
@@ -5,16 +5,17 @@ session_start(); // Starting Session
 
 if (!isset($_SESSION['username'])) {
     header('Location: login.php'); // Redirecting To Home Page
-} else {
-    //ADICIONAR PASSWORD
-    $rows = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? AND [Id] = ?", array($_SESSION['username'], $_SESSION['id']), "rows");
-    foreach ($rows as $row) {
-        if ($row['Username'] == $_SESSION['username']) {
-            
-        } else {
-            
-        }
-    }
 }
+//} else {
+//    //ADICIONAR PASSWORD
+//    $rows = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? AND [Id] = ?", array($_SESSION['username'], $_SESSION['id']), "rows");
+//    foreach ($rows as $row) {
+//        if ($row['Username'] == $_SESSION['username']) {
+//            
+//        } else {
+//            
+//        }
+//    }
+//}
 
 
