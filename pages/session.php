fafbeefd28aa8551cd51d847b110ad7f9ccf79a2
prcunha <prcunha.383@gmail.com>
2016-06-30 01:22:43 +0100

    FUNC TUDO OKI!

--- pages/session.php
+++ pages/session.php
@@ -6,7 +6,7 @@ session_start(); // Starting Session
 $userId = $_SESSION['id'];
 $userUsername = $_SESSION['username'];
 //Query To Fetch Complete Information Of User
-$user = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? and [Id] = ?", array($userUsername, $userId), "rows");
+$rows = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? and [Id] = ?", array($userUsername, $userId), "rows");
 foreach ($rows as $row) {
     if ($_SESSION['username'] == $row['Username']) {
         if (!isset($_SESSION['username'])) {
