4e88facd952f66049a082d606ea27feccb7ff10a
prcunha <prcunha.383@gmail.com>
2016-06-30 01:37:53 +0100

    FUNC TUDO OKI!

--- pages/profile.php
+++ pages/profile.php
@@ -1,38 +1,21 @@
 <?php
 //
-#include('session.php');
+include('session.php');
 #session_start();
 ?>
-<!--<!DOCTYPE html>
-<html>
-    <head>
-        <title>Your Home Page</title>
-        <link href="style.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <div id="profile">
-            <b id="welcome">Welcome : <i><?php echo $_SESSION['username']; ?></i></b>
-            <b id="logout"><a href="logout.php">Log Out</a></b>
-        </div>
-    </body>
-</html>-->
 
+//<?php session_start(); ?>
 
-<?php session_start(); ?>
-
-<html>
+<!--<html>
     <head>
         <title> Home </title>
     </head>
     <body>
-        <?php
-        if (!isset($_SESSION['username'])) { // If session is not set that redirect to Login Page                            {
-            header("Location:Login.php");
-        } else {
-            echo 'Welcome' . $_SESSION['username'] . '<br>'; 
-            #echo "Login Success";
-            echo "<a href='logout.php'> Logout</a> ";
-        }
-        ?>
-    </body>
-</html>
+-->
+<?php
+echo 'Welcome ' . $_SESSION['username'] . '<br>';
+#echo "Login Success";
+echo "<a href='logout.php'> Logout</a> ";
+?>
+</body>
+</html>-->
