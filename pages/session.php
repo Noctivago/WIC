a6c22a910ae36159deda695415e57b3ec5b38f5e
prcunha <prcunha.383@gmail.com>
2016-06-30 01:28:01 +0100

    FUNC TUDO OKI!

--- pages/session.php
+++ pages/session.php
@@ -2,18 +2,27 @@
 
 include_once '../db/conn.inc.php../';
 session_start(); // Starting Session
-//Storing Session
-$userId = $_SESSION['id'];
-$userUsername = $_SESSION['username'];
+
+if (!isset($_SESSION['username'])) {
+    #$pdo = null;
+    header('Location: login.php'); // Redirecting To Home Page
+} else {
+    //Storing Session
+    $userId = $_SESSION['id'];
+    $userUsername = $_SESSION['username'];
 //Query To Fetch Complete Information Of User
-$rows = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? AND [Id] = ?", array($userUsername, $userId), "rows");
-foreach ($rows as $row) {
-    if ($row['Username'] == $userUsername) {
-        if (!isset($_SESSION['username'])) {
-            #$pdo = null;
-            header('Location: login.php'); // Redirecting To Home Page
-        } else {
-            
+    $rows = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? AND [Id] = ?", array($userUsername, $userId), "rows");
+    foreach ($rows as $row) {
+        if ($row['Username'] == $userUsername) {
+            if (!isset($_SESSION['username'])) {
+                #$pdo = null;
+                header('Location: login.php'); // Redirecting To Home Page
+            } else {
+                
+            }
         }
     }
 }
+
+
+
