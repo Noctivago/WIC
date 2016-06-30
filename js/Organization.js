849d05eaf58f8c8ae1cb7595b62d825820f06355
Jesualdo Teixeira da Cunha <jesualdocunha@gmail.com>
2016-06-30 14:20:46 +0100

    add origanization and organizationService js

---
+++ js/Organization.js
@@ -0,0 +1,48 @@
+
+function addOrganization(){
+    
+}
+
+function removeOrganization(){
+    
+}
+
+function editOrganizationInformation(){
+    
+}
+
+function validateOrganization(){
+    
+}
+
+function assignUserInOrganization(){
+    
+}
+
+function removeUserInOrganization(){
+    
+}
+
+function UserValidateInvite(){
+    
+}
+
+function assignOrganizationCategoryOwner(){
+    
+}
+
+function assignOrganizationSubCategoryOwner(){
+    
+}
+function removeOrganizationCategoryOwner(){
+    
+}
+
+function removeOrganizationSubCategoryOwner(){
+    
+}
+
+function editPermissionUserInOrganization(){
+    
+}
+
---
+++ js/OrganizationService.js
@@ -0,0 +1,46 @@
+function addServiceOrganization(){
+    
+}
+function editServiceOrganization(){
+    
+}
+function removeServiceOrganization(){
+    
+}
+function assignUserInServiceOrganization(){
+    
+}
+function removeUserInServiceOrganization(){
+    
+}
+function createBookServiceOrganization(){
+    
+}
+function editBookServiceOrganization(){
+    
+}
+function removeBookServiceOrganization(){
+    
+}
+function addMultimediaOnBookServiceOrganization(){
+    
+}
+function removeMultimediaOnBookServiceOrganization(){
+    
+}
+function editMultimediaOnBookServiceOrganization(){
+    
+}
+
+function addCommentInBookService(){
+    
+}
+function removeCommentInBookService(){
+    
+}
+function viewCommentsBookService(){
+    
+}
+function evaluateService(){
+    
+}
