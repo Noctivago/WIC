f32276ffdc95a2695c64f91c854a91cb4b691ce3
prcunha <prcunha.383@gmail.com>
2016-06-30 19:14:42 +0100

    LOGIN -> LOGOUT -> SESSION & PROFILE

--- ajaxsubmit.php
+++ ajaxsubmit.php
@@ -11,14 +11,15 @@ include_once './db/conn.inc.php';
 $arg = (filter_var($_POST ['arg'], FILTER_SANITIZE_STRING));
 
 if ($arg === 'addUser') {
+    $username = (filter_var($_POST ['username'], FILTER_SANITIZE_STRING));
+    $password = (filter_var($_POST ['password'], FILTER_SANITIZE_STRING));
+    $email = (filter_var($_POST ['email'], FILTER_SANITIZE_EMAIL));
+    $hashPassword = hash('whirlpool', $password);
     if (checkIfUserExists($pdo, $email)) {
         echo "USER " . $username . " ALREADY EXISTS! FORGOT YOUR PASSWORD?!";
     } else {
         try {
-            $username = (filter_var($_POST ['username'], FILTER_SANITIZE_STRING));
-            $password = (filter_var($_POST ['password'], FILTER_SANITIZE_STRING));
-            $email = (filter_var($_POST ['email'], FILTER_SANITIZE_EMAIL));
-            $hashPassword = hash('whirlpool', $password);
+
             sql($pdo, "INSERT INTO [dbo].[User] ([Username], [Password], [Email]) VALUES (?, ?, ?)", array($username, $hashPassword, $email));
             echo "USER " . $username . " ADDED!";
         } catch (Exception $ex) {
