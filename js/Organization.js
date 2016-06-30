4f133dcfb29df32a9a5336ee2428ddcf5bd715d0
Jesualdo Teixeira da Cunha <jesualdocunha@gmail.com>
2016-06-30 14:58:05 +0100

    add functions orgsubmit

--- js/Organization.js
+++ js/Organization.js
@@ -1,6 +1,30 @@
-
 function addOrganization(){
-    
+    alert('AddOrg');
+    var name = $("#name").val();
+    var phone = $("#phone").val();
+    var mobile = $("#mobile").val();
+    var logotype = $("#logotype").val();
+    var address = $("#Address").val();
+    var facebook= $("#facebook").val();
+    var twitter = $("#twitter").val();
+    var linkdin = $("#linkdin").val();
+    var orgEmail = $("#email").val();
+    var website = $("#website").val();
+    var arg = 'addOrganization';
+    var dataString = 'name'+name+'&phone'+phone+'&moble'+mobile+'&logtype'+logotype+'&addres'+address+'&facebook'+facebook+'&twitter'+twitter+'&linkdin'+linkdin+'&orgEmail'+orgEmail+'&website'+'arg'+arg;
+    if(name===''||phone===''||mobile===''||logotype===''||address===''||orgEmail=''){
+        alert('Please fill All Fields');
+    }else
+    {
+        $.ajax({
+            type:"POST",
+            url:"orgsubmit.php",
+            data:dataString,
+            cache:false,sucess:function(result){
+                alert(result);
+            }
+        });
+    }
 }
 
 function removeOrganization(){
---
+++ orgsubmit.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once './db/conn.inc.php';
+
+$arg = (filter_var($_POST['arg'],FILTER_SANITIZE_STRING));
+
+if($arg==='addOrganization'){
+    
+    
+}else if ($arg==='removeOrganization') {
+    
+}else if ($arg==='editOrganizationInformation') {
+
+    
+}else if ($arg==='validateOrganization') {
+
+    
+}else if ($arg==='assignUserInOrganization') {
+
+    
+}else if ($arg==='removeUserInOrganization') {
+
+    
+}else if ($arg==='UserValidateInvite') {
+
+    
+}else if ($arg==='assignOrganizationCategoryOwner') {
+
+    
+}else if ($arg==='assignOrganizationSubCategoryOwner') {
+    
+}else if ($arg==='removeOrganizationCategoryOwner') {
+
+    
+}else if ($arg==='removeOrganizationSubCategoryOwner') {
+
+    
+}else if ($arg==='editPermissionUserInOrganization') {
+
+    
+}else if ($arg==='viewAllOrganizations') {
+
+    
+}else if ($arg==='viewAllUsersInOrganization') {
+    
+}
