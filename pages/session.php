a1a2a31b22b9704b782cf641df33c78007f907e5
prcunha <prcunha.383@gmail.com>
2016-06-30 01:05:15 +0100

    FUNC TUDO OKI!

--- pages/session.php
+++ pages/session.php
@@ -3,8 +3,8 @@
 include_once '../db/conn.inc.php../';
 session_start(); // Starting Session
 //Storing Session
-$userId = $_SESSION['Id'];
-$userUsername = $_SESSION['Username'];
+$userId = $_SESSION['id'];
+$userUsername = $_SESSION['username'];
 //Query To Fetch Complete Information Of User
 $user = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Username] = ? and [Id] = ?", array($userUsername, $userId), "rows");
 foreach ($rows as $row) {
