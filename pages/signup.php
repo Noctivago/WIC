8086d57d998c22fe4e06e1790bab53e4e571fdf7
prcunha <prcunha.383@gmail.com>
2016-06-30 20:51:42 +0100

    LOGIN -> LOGOUT -> SESSION & PROFILE

--- pages/signup.php
+++ pages/signup.php
@@ -87,8 +87,9 @@ include_once '../db/functions.php';
                     $forgotPassword = '<a href=account-recovery.php>Forgot your account details?</a>';
                 } else {
                     try {
-                        $length = '128';
-                        $code = generateActivationCode($length);
+                        //$length = '128';
+                        //$code = generateActivationCode($length);
+                        $code = 'generateActivationCode($length)';
                         echo $code;
                         sql($pdo, "INSERT INTO [dbo].[User] ([Username], [Password], [Email], [Account_Enabled], [User_Code_Activation]) VALUES (?, ?, ?, ?, ?)", array($username, $hashPassword, $email, '0', $code));
                         $msg = 'USER ' . $username . ' ADDED!';
