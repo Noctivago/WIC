00ef5d1ae93980d7201d8dbf9dbe632ace0b4f32
prcunha <prcunha.383@gmail.com>
2016-06-30 22:45:44 +0100

    LOGIN -> LOGOUT -> SESSION & PROFILE -> GENERATE ACTIVATION CODE -> ACTIVATING ACCOUNT

--- db/conn.inc.php
+++ db/conn.inc.php
@@ -68,9 +68,9 @@ function DB_compareActivationCode($pdo, $email, $code) {
     try {
         $count = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Email] = ? AND [User_Code_Activation] = ?", array($email, $code), "count");
         if ($count < 0) {
-            return true;
-        } else {
             return false;
+        } else {
+            return true;
         }
     } catch (Exception $exc) {
         echo '';
