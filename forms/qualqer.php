67344d7d9eb091c862e1ccd43cfd9e06db05ca1c
prcunha <prcunha.383@gmail.com>
2016-07-03 00:41:35 +0100

    STARTING PERMISSIONS

--- forms/qualqer.php
+++ forms/qualqer.php
@@ -33,115 +33,114 @@
 
     </head>
 
-    <body  onload="readOrganizationPermissons()>
-        
-        <!-- Top content -->
-        <div class="top-content">
+    <body  onload="readOrganizationPermissons() >
+                    < !-- Top content -- >
+                    < div class ="top-content">
 
-            <div class="inner-bg">
+           <div class="inner-bg">
+            <div class="container">
                 <div class="container">
-                    <div class="container">
-                        <?php
-                        if (isset($_POST['login']) && !empty($_POST['email']) && !empty($_POST['password'])) {
-                            try {
-                                $msg = '';
-                                $email = (filter_var($_POST ['email'], FILTER_SANITIZE_EMAIL));
-                                $password = (filter_var($_POST ['password'], FILTER_SANITIZE_STRING));
-                                $hashPassword = hash('whirlpool', $password);
-                                if (DB_checkIfUserExists($pdo, $email)) {
-                                    $rows = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Email] = ? and [Account_Enabled] = ?", array($email, '1'), "rows");
-                                    #$msg ='EMAIL FOUND';
-                                    foreach ($rows as $row) {
-                                        if ($row['Email'] == $email && $row['Password'] == $hashPassword) {
-                                            //ADICIONAR PASSWORD
-                                            $_SESSION['valid'] = true;
-                                            $_SESSION['timeout'] = time();
-                                            $_SESSION['id'] = $row['Id'];
-                                            $_SESSION['username'] = $row['Username'];
-                                            $_SESSION['email'] = $row['Email'];
-                                            $_SESSION['password'] = $row['Password'];
-                                            $msg = 'Welcome ' . $row['Username'];
-                                            //SET [Login_failed] = 0
-                                            if (DB_setLoginFailed($pdo, $email)) {
-                                                header('Location: profile.php');
-                                            }
+                    <?php
+                    if (isset($_POST['login']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+                        try {
+                            $msg = '';
+                            $email = (filter_var($_POST ['email'], FILTER_SANITIZE_EMAIL));
+                            $password = (filter_var($_POST ['password'], FILTER_SANITIZE_STRING));
+                            $hashPassword = hash('whirlpool', $password);
+                            if (DB_checkIfUserExists($pdo, $email)) {
+                                $rows = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Email] = ? and [Account_Enabled] = ?", array($email, '1'), "rows");
+                                #$msg ='EMAIL FOUND';
+                                foreach ($rows as $row) {
+                                    if ($row['Email'] == $email && $row['Password'] == $hashPassword) {
+                                        //ADICIONAR PASSWORD
+                                        $_SESSION['valid'] = true;
+                                        $_SESSION['timeout'] = time();
+                                        $_SESSION['id'] = $row['Id'];
+                                        $_SESSION['username'] = $row['Username'];
+                                        $_SESSION['email'] = $row['Email'];
+                                        $_SESSION['password'] = $row['Password'];
+                                        $msg = 'Welcome ' . $row['Username'];
+                                        //SET [Login_failed] = 0
+                                        if (DB_setLoginFailed($pdo, $email)) {
+                                            header('Location: profile.php');
+                                        }
+                                    } else {
+                                        //INC TO BLOCK;
+                                        //SET
+                                        $val = DB_getLoginFailedValue($pdo, $email);
+                                        if ($val < 3) {
+                                            $value = $val + 1;
+                                            DB_setLoginFailed($pdo, $email, $value);
+                                            $msg = 'Wrong username or password';
                                         } else {
-                                            //INC TO BLOCK;
-                                            //SET
-                                            $val = DB_getLoginFailedValue($pdo, $email);
-                                            if ($val < 3) {
-                                                $value = $val + 1;
-                                                DB_setLoginFailed($pdo, $email, $value);
-                                                $msg = 'Wrong username or password';
-                                            } else {
-                                                //BLOCK ACCOUNT
-                                                DB_setLoginFailed($pdo, $email);
-                                                DB_setBlockAccount($pdo, $email);
-                                                $msg = 'Account blocked!';
-                                            }
+                                            //BLOCK ACCOUNT
+                                            DB_setLoginFailed($pdo, $email);
+                                            DB_setBlockAccount($pdo, $email);
+                                            $msg = 'Account blocked!';
                                         }
                                     }
-                                } else {
-                                    $msg = 'Wrong username or password';
                                 }
-                            } catch (Exception $ex) {
-                                echo "ERROR!";
+                            } else {
+                                $msg = 'Wrong username or password';
                             }
+                        } catch (Exception $ex) {
+                            echo "ERROR!";
                         }
-                        ?>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-8 col-sm-offset-2 text">
+                    }
+                    ?>
+                </div>
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2 text">
 
-                        </div>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-5">
+                </div>
+                <div class="row">
+                    <div class="col-sm-5">
 
-                            <div class="form-box">
-                                <div class="form-top">
-                                    <!--FORM CHAPT-->
-                                </div>
-                                <div class="form-bottom">
-                                    <p>Permission description:</p>
-                                    <form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="login-form">
-                                        <div class="form-group">
-                                            <h4 <?php echo $msg; ?></h4>
-                                            <input type="text" name="permission" placeholder="type permission description..." class="form-username form-control" id="permissiondescription" required autofocus>
-                                        </div>
-                                        <input type="checkbox" name="permissionOrg" value="permissionOrg" /> Organization Permission?
-                                        <br>
-                                        <button type="submit" class="btn" name="addOrgPermission">Add permission!</button>
-                                    </form>
-                                </div>
+                        <div class="form-box">
+                            <div class="form-top">
+                                <!--FORM CHAPT-->
+                            </div>
+                            <div class="form-bottom">
+                                <p>Permission description:</p>
+                                <form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="login-form">
+                                    <div class="form-group">
+                                        <h4 <?php echo $msg; ?></h4>
+                                        <input type="text" id = "permission" name="permission" placeholder="type permission description..." class="form-username form-control" id="permissiondescription" required autofocus>
+                                    </div>
+                                    <input type="checkbox" id = "permissionOrg" name="permissionOrg" value="1" /> Organization Permission?
+                                    <br>
+                                    <button type="submit" class="btn" name="addOrgPermission">Add permission!</button>
+                                </form>
                             </div>
                         </div>
-                        <!--                        <div class="col-sm-1 middle-border"></div>-->
-                        <div class="col-sm-1"></div>
-
-                        <div class="col-sm-5">
-                            <p>Available Permissions</p>
-                            <div id="loadPermissionOrg"></div>
-                        </div>
                     </div>
+                    <!--                        <div class="col-sm-1 middle-border"></div>-->
+                    <div class="col-sm-1"></div>
 
+                    <div class="col-sm-5">
+                        <p>Available Permissions</p>
+                        <div id="loadPermissionOrg"></div>
+                    </div>
                 </div>
+
             </div>
         </div>
-        <!-- Footer -->
+    </div>
+    <!-- Footer -->
 
 
-        <!-- Javascript -->
-        <script src="../assets/js/jquery-1.11.1.min.js"></script>
-        <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-        <script src="../assets/js/jquery.backstretch.min.js"></script>
-        <script src="../assets/js/scripts.js"></script>
+    <!-- Javascript -->
+    <script src="../assets/js/jquery-1.11.1.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/js/jquery.backstretch.min.js"></script>
+    <script src="../assets/js/scripts.js"></script>
 
-        <!--[if lt IE 10]>
-            <script src="assets/js/placeholder.js"></script>
-        <![endif]-->
+    <!--[if lt IE 10]>
+        <script src="assets/js/placeholder.js"></script>
+    <![endif]-->
 
-    </body>
+</body>
 
 
 </html>
---
+++ js/OrganizationPermissions.js
@@ -0,0 +1,34 @@
+function addOrganizationService() {
+    var userid = $("#userid").val();
+    var name = $("#name").val();
+    var phone = $("#phone").val();
+    var mobile = $("#mobile").val();
+    //  var logotype = $("#logotype").val();
+    var address = $("#Address").val();
+    var facebook = $("#facebook").val();
+    var twitter = $("#twitter").val();
+    var linkdin = $("#linkdin").val();
+    var orgEmail = $("#email").val();
+    var website = $("#website").val();
+    var arg = 'addOrganization';
+    var dataString = 'userid=' + userid + '&name=' + name + '&phone=' + phone + '&mobile=' + mobile + '&address=' + address + '&facebook=' + facebook + '&twitter=' + twitter + '&linkdin=' + linkdin + '&orgEmail=' + orgEmail + '&website=' + website + '&arg=' + arg;
+    alert(dataString);
+    if (name === '' || mobile === '' || address === '' || orgEmail === '') {
+        alert('Please filld the fields required <br/>Name<br/>mobile<br/>Adress<br/>Organization Email');
+    } else
+    {
+        $.ajax({
+            type: "POST",
+            url: "orgsubmit.php",
+            data: dataString,
+            cache: false,
+            sucess: function (result) {
+                var ajaxDisplay = document.getElementById('result');
+                ajaxDisplay.innerHTML = result;
+                alert(result);
+            }
+        });
+        viewAllOrganization();
+        return false;
+    }
+}
