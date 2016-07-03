c2a0600b35c364f5a5e57d4b6437d14b5c8e6c33
prcunha <prcunha.383@gmail.com>
2016-07-03 23:32:38 +0100

    STARTING PERMISSIONS

--- forms/OrgManagement.php
+++ forms/OrgManagement.php
@@ -4,9 +4,10 @@ include_once '../db/conn.inc.php';
 /*
  * SE BOSS PODE FAZER TUDO
  */
-
+echo 'CATEGORY';
 DB_getCategoryAsTable($pdo);
 DB_getCategoryAsSelect($pdo);
+echo 'SUBCATEGORY';
 DB_getSubCategoryAsTable($pdo);
 DB_getSubCategoryAsSelect($pdo);
 
