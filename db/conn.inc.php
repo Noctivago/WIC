1b0c926ee6d93acb1075c8d507ff2c93514d0290
prcunha <prcunha.383@gmail.com>
2016-07-04 22:34:24 +0100

    STARTING PERMISSIONS

--- db/conn.inc.php
+++ db/conn.inc.php
@@ -508,7 +508,7 @@ function DB_readOrganizationServiceAsSelect($pdo, $userId) {
 function DB_addOrganizationServiceBook($pdo, $name, $description, $d, $orgSerId) {
     try {
         sql($pdo, "INSERT INTO [dbo].[Organization_Service_Book] ([Name],[Description],[Organization_Id],[Date_Created], [Enabled]"
-                . "VALUES(?,?,?,?,?)", array($name, $description, $orgSerId, $d, 1));
+                . "VALUES(?,?,?,?,?)", array($name, $description, $orgSerId, $d, '1'));
         echo 'Organization Service Book added!';
     } catch (PDOException $e) {
         print "Error!" . "<br/>";
