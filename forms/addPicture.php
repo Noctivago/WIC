2873a722bc2c1bde322b59992ea1a1184a3afd75
prcunha <prcunha.383@gmail.com>
2016-07-05 21:09:07 +0100

    STARTING PERMISSIONS

--- forms/addPicture.php
+++ forms/addPicture.php
@@ -68,7 +68,7 @@ include_once ('../db/functions.php');
                                         date_default_timezone_set("Europe/Lisbon"); //Definindo timezone padrão
                                         $ext = strtolower(substr($_FILES['fileUpload']['name'], -4)); //Pegando extensão do arquivo
                                         $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
-                                        $dir = 'uploads/'; //Diretório para uploads
+                                        $dir = '../pics/'; //Diretório para uploads
                                         move_uploaded_file($_FILES['fileUpload']['tmp_name'], $dir . $new_name); //Fazer upload do arquivo
                                     }
                                     ?>
