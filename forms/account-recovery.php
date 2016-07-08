dd8ab7b2449d0f70569ee2664e82d1304c713d88
prcunha <prcunha.383@gmail.com>
2016-07-08 21:02:45 +0100

    STARTING PERMISSIONS

--- forms/account-recovery.php
+++ forms/account-recovery.php
@@ -3,8 +3,8 @@ include_once '../db/conn.inc.php';
 include_once '../db/functions.php';
 ?>
 <?
-// error_reporting(E_ALL);
-// ini_set("display_errors", 1);
+ error_reporting(E_ALL);
+ ini_set("display_errors", 1);
 ?>
 
 <!DOCTYPE html>
