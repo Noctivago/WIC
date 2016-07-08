1735ed8dd58e6fa83111be76a9d0277e84e57542
prcunha <prcunha.383@gmail.com>
2016-07-08 22:16:52 +0100

    STARTING PERMISSIONS

--- forms/addPicture.php
+++ forms/addPicture.php
@@ -65,12 +65,13 @@ include_once ('../db/functions.php');
                                 <div class="form-bottom">
                                     <?php
                                     $msg = '';
-                                    $target_dir = "../pics/";
-                                    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-                                    $uploadOk = 1;
-                                    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
 // Check if image file is a actual image or fake image
                                     if (isset($_POST["submit"])) {
+                                        $target_dir = "../pics/";
+                                        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+                                        $uploadOk = 1;
+                                        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
                                         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
                                         if ($check !== false) {
                                             echo "File is an image - " . $check["mime"] . ".";
