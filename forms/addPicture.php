b098ce0e16de335144432466b08db7e78d1c9110
prcunha <prcunha.383@gmail.com>
2016-07-08 22:12:50 +0100

    STARTING PERMISSIONS

--- forms/addPicture.php
+++ forms/addPicture.php
@@ -65,23 +65,60 @@ include_once ('../db/functions.php');
                                 <div class="form-bottom">
                                     <?php
                                     $msg = '';
-                                    if (isset($_FILES['fileUpload'])) {
-                                        date_default_timezone_set("Europe/Lisbon"); //Definindo timezone padrão
-                                        $ext = strtolower(substr($_FILES['fileUpload']['name'], -4)); //Pegando extensão do arquivo
-                                        $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
-                                        $dir = '../pics/'; //Diretório para uploads
-                                        $msg = move_uploaded_file($_FILES['fileUpload']['tmp_name'], $dir . $new_name); //Fazer upload do arquivo
+                                    $target_dir = "../pics/";
+                                    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+                                    $uploadOk = 1;
+                                    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+                                    if (isset($_POST["submit"])) {
+                                        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+                                        if ($check !== false) {
+                                            echo "File is an image - " . $check["mime"] . ".";
+                                            $uploadOk = 1;
+                                        } else {
+                                            echo "File is not an image.";
+                                            $uploadOk = 0;
+                                        }
+                                    }
+// Check if file already exists
+                                    if (file_exists($target_file)) {
+                                        echo "Sorry, file already exists.";
+                                        $uploadOk = 0;
+                                    }
+// Check file size
+                                    if ($_FILES["fileToUpload"]["size"] > 500000) {
+                                        echo "Sorry, your file is too large.";
+                                        $uploadOk = 0;
+                                    }
+// Allow certain file formats
+                                    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+                                        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                                        $uploadOk = 0;
+                                    }
+// Check if $uploadOk is set to 0 by an error
+                                    if ($uploadOk == 0) {
+                                        echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
                                     } else {
-                                        $msg = 'ERROR UPLOADING THE PICTURE!';
+                                        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                                            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+                                        } else {
+                                            echo "Sorry, there was an error uploading your file.";
+                                        }
                                     }
                                     ?>
                                     <div class = "form-group"><h4> <?php echo $msg; ?></h4>
                                         <label for="file"><span class="IL_AD" id="IL_AD4">Filename</span>:</label>
                                         <input type="file" name="fileUpload" id="fileUpload"><br>
+
+                                        <!-- ESCOLHER SERVIÇO AO QUAL SERA ADICIONADA A PIC -->
+                                        <button type="submit" class="btn" name="addPicture">UPLOAD PICTURE!</button>
+                                        <form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data" class="login-form">
+                                            Select image to upload:
+                                            <input type="file" name="fileToUpload" id="fileToUpload">
+                                            <input type="submit" value="Upload Image" name="submit">
+                                        </form>
                                     </div>
-                                    <!-- ESCOLHER SERVIÇO AO QUAL SERA ADICIONADA A PIC -->
-                                    <button type="submit" class="btn" name="addPicture">UPLOAD PICTURE!</button>
-                                    </form>
                                 </div>
                             </div>
 
