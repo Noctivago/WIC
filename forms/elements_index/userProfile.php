1f7874a94ae3a91b990a561cc35eaaba6e836fe1
prcunha <prcunha.383@gmail.com>
2016-07-09 23:15:29 +0100

    STARTING PERMISSIONS

--- forms/elements_index/userProfile.php
+++ forms/elements_index/userProfile.php
@@ -63,7 +63,8 @@ include_once ('../db/functions.php');
                                             $lname = (filter_var($_POST ['lname'], FILTER_SANITIZE_STRING));
                                             $countryId = (filter_var($_POST ['country'], FILTER_SANITIZE_NUMBER_INT));
                                             $userId = $_SESSION['id'];
-                                            $msg = DB_updateUserProfile($pdo, $fname, $lname, $countryId, $userId);
+                                            #$msg = DB_updateUserProfile($pdo, $fname, $lname, $countryId, $userId);
+                                            $msg = DB_updateUserProfile($pdo, $fname, $lname, $userId);
                                         }
                                         ?>
                                         <?= DB_getUserProfilePicture($pdo, $_SESSION['id']) ?>
