d379da0bc5573d635763986b31ee6d34105e2132
prcunha <prcunha.383@gmail.com>
2016-07-09 12:13:08 +0100

    STARTING PERMISSIONS

--- forms/addPicture.php
+++ forms/addPicture.php
@@ -79,26 +79,25 @@ include_once ('../db/conn.inc.php');
                                             echo "File is not an image.";
                                             $uploadOk = 0;
                                         }
-
-// Check if file already exists
-                                        if (file_exists($target_file)) {
-                                            echo "Sorry, file already exists.";
-                                            $uploadOk = 0;
-                                        }
-// Check file size
+                                        // Check if file already exists
+//                                        if (file_exists($target_file)) {
+//                                            echo "Sorry, file already exists.";
+//                                            $uploadOk = 0;
+//                                        }
+                                        // Check file size
                                         if ($_FILES["fileToUpload"]["size"] > 500000) {
                                             echo "Sorry, your file is too large.";
                                             $uploadOk = 0;
                                         }
-// Allow certain file formats
+                                        // Allow certain file formats
                                         if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
                                             echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
                                             $uploadOk = 0;
                                         }
-// Check if $uploadOk is set to 0 by an error
+                                        // Check if $uploadOk is set to 0 by an error
                                         if ($uploadOk == 0) {
                                             echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+                                            // if everything is ok, try to upload file
                                         } else {
                                             #if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                                             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $_SESSION['id'] . '.jpg')) {
