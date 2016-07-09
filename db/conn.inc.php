d53be7ff9ef3c4b83a6341efc1e6628a4743118f
prcunha <prcunha.383@gmail.com>
2016-07-09 16:01:27 +0100

    STARTING PERMISSIONS

--- db/conn.inc.php
+++ db/conn.inc.php
@@ -720,7 +720,8 @@ function DB_getUserProfilePicture($pdo, $userId) {
         #foreach ($rows as $row) {
         #    return $row['Picture_Path'];
         if ($rows['Picture_Path'] == NULL) {
-            return "PROFILE PICTURE NOT FOUND!";
+            #return "PROFILE PICTURE NOT FOUND!";
+            return '<img src=/"http://lyco.com.br/site/empresa/images/icone_grande_empresa-2.png/" class=/"avatar img-circle img-thumbnail text-center center-block/" alt=/"avatar/">';
         } else {
             return $rows['Picture_Path'];
         }
