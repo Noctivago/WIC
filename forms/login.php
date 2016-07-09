96bf2cc2c8e149394dc296285c4d849a25332fcd
prcunha <prcunha.383@gmail.com>
2016-07-09 11:39:27 +0100

    STARTING PERMISSIONS

--- forms/login.php
+++ forms/login.php
@@ -162,7 +162,7 @@ if (isset($_SESSION['username'])) {
                                             <label class="sr-only" for="form-password">Password</label>
                                             <input type="password" name="password" placeholder="Password" class="form-password form-control" id="form-password" required>
                                         </div>
-                                        <div class="g-recaptcha" id="g-recaptcha-response" data-sitekey="6LdypyQTAAAAACjs5ZFCy67r2JXYJUcudQvstby6"></div>
+                                        <div class="g-recaptcha" data-sitekey="6LdypyQTAAAAACjs5ZFCy67r2JXYJUcudQvstby6"></div>
                                         <button type="submit" class="btn" name="login">Sign in!</button>
 
                                     </form>
