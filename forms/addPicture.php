a5736b56219761c7d83819a97134457768817db9
prcunha <prcunha.383@gmail.com>
2016-07-09 15:18:41 +0100

    STARTING PERMISSIONS

--- forms/addPicture.php
+++ forms/addPicture.php
@@ -5,8 +5,8 @@ include_once ('../db/functions.php');
 ?>
 
 <?
-// error_reporting(E_ALL);
-// ini_set("display_errors", 1);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 ?>
 <!DOCTYPE html>
 <html lang="en">
