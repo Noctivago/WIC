bb1830968522f1765bbda61973c5227fe93cb2fc
prcunha <prcunha.383@gmail.com>
2016-07-09 12:09:19 +0100

    STARTING PERMISSIONS

--- forms/addPicture.php
+++ forms/addPicture.php
@@ -64,11 +64,11 @@ include_once ('../db/conn.inc.php');
                                 <div class="form-bottom">
                                     <?php
                                     $msg = '';
-
-// Check if image file is a actual image or fake image
+                                    // Check if image file is a actual image or fake image
                                     if (isset($_POST["submit"])) {
                                         $target_dir = "../pics/";
-                                        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+                                        $target_file = $target_dir . '36.jpg'; #basename($_FILES["fileToUpload"]["name"]);
+                                        #$target_file = $target_dir . basename($_FILES["fileToUpload"][$_SESSION['id']]);
                                         $uploadOk = 1;
                                         $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
                                         $check = getimagesize($_FILES["fileToUpload"]["name"]);
@@ -100,7 +100,7 @@ include_once ('../db/conn.inc.php');
                                             echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
                                         } else {
-                                            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $_SESSION['id'] . 'jpg')) {
+                                            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                                                 echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
                                             } else {
                                                 echo "Sorry, there was an error uploading your file.";
