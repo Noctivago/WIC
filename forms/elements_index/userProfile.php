713a751e7f0e0a8cef98a707d6b21c9b5c8cabd2
prcunha <prcunha.383@gmail.com>
2016-07-10 01:20:08 +0100

    STARTING PERMISSIONS

--- forms/elements_index/userProfile.php
+++ forms/elements_index/userProfile.php
@@ -75,7 +75,7 @@ $userInfo = array();
                                         </div>
                                         <div col-sm-6 col-sm-offset-2 text>
                                             <form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data" class="login-form">
-                                                Select image to upload:
+                                                <!--Select image to upload:-->
                                                 <input type="file" name="Photo" id="Photo" required="">
                                                 <button type="submit" class ="btn" name="changePic">Change picture!</button>
                                             </form>
