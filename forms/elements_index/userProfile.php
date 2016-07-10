d9ba0045ad3636a64d83636bdac42b8dc9f6f545
prcunha <prcunha.383@gmail.com>
2016-07-10 01:04:51 +0100

    STARTING PERMISSIONS

--- forms/elements_index/userProfile.php
+++ forms/elements_index/userProfile.php
@@ -71,7 +71,6 @@ $userInfo = array();
                                         ?>
                                         <?= DB_getUserProfilePicture($pdo, $_SESSION['id']) ?>
                                         <?= $userInfo = DB_getUserProfileInfo($pdo, $_SESSION['id']) ?>
-                                        <br>
                                         <form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data" class="login-form">
                                             Select image to upload:
                                             <input type="file" name="Photo" id="Photo" required="">
