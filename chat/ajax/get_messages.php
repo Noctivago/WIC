72739dae296cd4d58f0d42e57fe22b15ed6de05f
prcunha <prcunha.383@gmail.com>
2016-07-10 14:13:40 +0100

    STARTING CHAT

--- chat/ajax/get_messages.php
+++ chat/ajax/get_messages.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once __DIR__ . '/../../core/FbChatMock.php';
+require_once __DIR__ . '/../../db/FbChatMock.php';
 
 $chat = new FbChatMock();
 $messages = $chat->getMessages();
 $chat_converstaion = array();
 
 if (!empty($messages)) {
-  $chat_converstaion[] = '<table>';
-  foreach ($messages as $message) {
-    $msg = htmlentities($message['message'], ENT_NOQUOTES);
-    $user_name = ucfirst($message['username']);
-    $sent = date('F j, Y, g:i a', $message['sent_on']);
-    $chat_converstaion[] = <<<MSG
+    $chat_converstaion[] = '<table>';
+    foreach ($messages as $message) {
+        $msg = htmlentities($message['message'], ENT_NOQUOTES);
+        $user_name = ucfirst($message['username']);
+        $sent = date('F j, Y, g:i a', $message['sent_on']);
+        $chat_converstaion[] = <<<MSG
       <tr class="msg-row-container">
         <td>
           <div class="msg-row">
@@ -24,11 +24,10 @@ if (!empty($messages)) {
         </td>
       </tr>
 MSG;
-  }
-  $chat_converstaion[] = '</table>';
+    }
+    $chat_converstaion[] = '</table>';
 } else {
-  echo '<span style="margin-left: 25px;">No chat messages available!</span>';
+    echo '<span style="margin-left: 25px;">No chat messages available!</span>';
 }
 
 echo implode('', $chat_converstaion);
-?>
--- db/FbChatMock.php
+++ db/FbChatMock.php
@@ -7,42 +7,44 @@
  */
 class FbChatMock {
 
-  // Holds the database connection
-  private $dbConnection;
-  
-  //----- Database connection details --/
-  //-- Change these to your database values
-  
-  private $_dbHost = 'localhost';
-  
-  private $_dbUsername = 'root';
-  
-  private $_dbPassword = 'root';
-  
-  public $_databaseName = 'fbchat';
-  
-  //----- ----/
+    // Holds the database connection
+    private $dbConnection;
+    //----- Database connection details --/
+    //-- Change these to your database values
 
-  /**
-   * Create's the connection to the database and stores it in the dbConnection
-   */
-  public function __construct() {
-    $this->dbConnection = new mysqli($this->_dbHost, $this->_dbUsername, 
-        $this->_dbPassword, $this->_databaseName);
+    private $_dbHost = 'wicsqlserver.database.windows.net';
+    private $_dbUsername = 'wic';
+    private $_dbPassword = '#$youcandoit2017$#';
+    public $_databaseName = 'wicdb';
 
-    if ($this->dbConnection->connect_error) {
-      die('Connection error.');
+    //----- ----/
+
+    /**
+     * Create's the connection to the database and stores it in the dbConnection
+     */
+    public function __construct() {
+//        $this->dbConnection = new mysqli($this->_dbHost, $this->_dbUsername, $this->_dbPassword, $this->_databaseName);
+//
+//        if ($this->dbConnection->connect_error) {
+//            die('Connection error.');
+//        }
+        $this->dbConnection = new PDO("sqlsrv:server=$_dbHost; Database=$_databaseName", "$_dbUsername", "$_dbPassword");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        if ($this->dbConnection->connect_error) {
+            die('Connection error.');
+        }
     }
-  }
 
-  /**
-   * Get the list of messages from the chat
-   * 
-   * @return array
-   */
-  public function getMessages() {
-    $messages = array();
-    $query = <<<QUERY
+    /**
+     * Get the list of messages from the chat
+     * 
+     * @return array
+     */
+    public function getMessages() {
+        $messages = array();
+        $query = <<<QUERY
         SELECT 
           `chat`.`message`, 
           `chat`.`sent_on`,
@@ -53,46 +55,46 @@ class FbChatMock {
           ON `chat`.`user_id` = `users`.`id`
         ORDER BY `sent_on`
 QUERY;
-    
-    // Execute the query
-    $resultObj = $this->dbConnection->query($query);
-    // Fetch all the rows at once.
-    while ($row = $resultObj->fetch_assoc()) {
-      $messages[] = $row;
-    }
-    
-    return $messages;
-  }
 
-  /**
-   * Add a new message to the chat table
-   * 
-   * @param Integer $userId The user who sent the message
-   * @param String $message The Actual message
-   * @return bool|Integer The last inserted id of success and false on faileur
-   */
-  public function addMessage($userId, $message) {
-    $addResult = false;
-    
-    $cUserId = (int) $userId;
-    // Escape the message with mysqli real escape
-    $cMessage = $this->dbConnection->real_escape_string($message);
-    
-    $query = <<<QUERY
+        // Execute the query
+        $resultObj = $this->dbConnection->query($query);
+        // Fetch all the rows at once.
+        while ($row = $resultObj->fetch_assoc()) {
+            $messages[] = $row;
+        }
+
+        return $messages;
+    }
+
+    /**
+     * Add a new message to the chat table
+     * 
+     * @param Integer $userId The user who sent the message
+     * @param String $message The Actual message
+     * @return bool|Integer The last inserted id of success and false on faileur
+     */
+    public function addMessage($userId, $message) {
+        $addResult = false;
+
+        $cUserId = (int) $userId;
+        // Escape the message with mysqli real escape
+        $cMessage = $this->dbConnection->real_escape_string($message);
+
+        $query = <<<QUERY
       INSERT INTO `chat`(`user_id`, `message`, `sent_on`)
       VALUES ({$cUserId}, '{$cMessage}', UNIX_TIMESTAMP())
 QUERY;
 
-    $result = $this->dbConnection->query($query);
-    
-    if ($result !== false) {
-      // Get the last inserted row id.
-      $addResult = $this->dbConnection->insert_id;
-    } else {
-      echo $this->dbConnection->error;
+        $result = $this->dbConnection->query($query);
+
+        if ($result !== false) {
+            // Get the last inserted row id.
+            $addResult = $this->dbConnection->insert_id;
+        } else {
+            echo $this->dbConnection->error;
+        }
+
+        return $addResult;
     }
-    
-    return $addResult;
-  }
 
 }
