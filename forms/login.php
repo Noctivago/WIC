8525c5aa5090d90b7ec9f4cc56c1d9a295f0b630
prcunha <prcunha.383@gmail.com>
2016-07-11 19:55:04 +0100

    COUNTRY USER PROFILE

--- forms/login.php
+++ forms/login.php
@@ -179,7 +179,7 @@ if (isset($_SESSION['username'])) {
                                             <label class="sr-only" for="form-password">Password</label>
                                             <input type="password" name="password" placeholder="Password" class="form-password form-control" id="form-password" required>
                                         </div>
-                                        <div class="g-recaptcha" data-sitekey="6LdypyQTAAAAACjs5ZFCy67r2JXYJUcudQvstby6"></div>
+                                        <div class="g-recaptcha" data-sitekey="6LdypyQTAAAAACjs5ZFCy67r2JXYJUcudQvstby6" required></div>
                                         <br>
                                         <button type="submit" class="btn" name="login">Sign in!</button>
 
