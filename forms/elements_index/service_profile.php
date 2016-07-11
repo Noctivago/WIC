8726227d3096aa769959b225d84bcc91f7014f02
prcunha <prcunha.383@gmail.com>
2016-07-11 23:12:58 +0100

    SERVICE COMMENT

--- forms/elements_index/service_profile.php
+++ forms/elements_index/service_profile.php
@@ -74,9 +74,9 @@ $msg;
                                     <div class = "form-bottom">
                                         <!--<div class = "status-upload"> -->
                                         <div class = "cd-label">
-                                            <form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="login-form">
-                                                <div class="form-group"><h4> <?php echo $msg; ?></h4>
-                                                    <textarea id="userComment" placeholder="Write your comment here" style="width: 100%"></textarea>
+                                            <form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                                                <div><h4> <?php echo $msg; ?></h4>
+                                                    <textarea id="userComment" name ="userComment" placeholder="Write your comment here" style="width: 100%"></textarea>
                                                 </div>
                                                 <button type="submit" class="btn" id = "addComment" name="addComment"><i class="fa fa-reply"></i>POST </button>
                                         </div><!-- Status Upload class="btn btn-success "
