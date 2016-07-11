3b34bbb8658655e5ebc01f7c95f142a81a490d44
prcunha <prcunha.383@gmail.com>
2016-07-11 23:55:58 +0100

    SERVICE WIC

--- forms/elements_index/service_profile.php
+++ forms/elements_index/service_profile.php
@@ -62,6 +62,7 @@ $msg;
                                         <select class="form-username form-control" name="wicPlanner" id="wicPlanner" required="required">
                                             <?= DB_getWicPlannerAsSelect($pdo, $_SESSION['id']) ?>
                                         </select>
+                                        <br>
                                         <div class = "login-form">
                                             <button onclick="hide('showWICPlanner');show('showWICPlannerBTN');" class = "btn" name = "WICPlannerHide">Hide WIC Planner!</button>
                                             <button class = "btn" name = "addToWICPlanner">Add to WIC Planner!</button>
