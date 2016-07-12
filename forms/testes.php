a99ca316514fda4eed8d9261afc16a095145172a
prcunha <prcunha.383@gmail.com>
2016-07-12 22:27:26 +0100

    TESTES DEBUG START CONVERSATION

---
+++ forms/testes.php
@@ -0,0 +1,21 @@
+<?php
+//
+include_once ('session.php');
+include_once ('../db/conn.inc.php');
+#session_start();
+?>
+
+<html>
+    <head>
+        <title> Home </title>
+    </head>
+    <body>
+
+        <?php
+        echo 'Welcome ' . $_SESSION['username'] . '<br>';
+        $orgUsers = DB_checkUserToStartChat($pdo, $orgServId);
+        var_dump($orgUsers);
+        ?>
+
+    </body>
+</html>
