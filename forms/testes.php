2f95276b0a7bf1cd728c5f0b9bd3902d381f0cd5
prcunha <prcunha.383@gmail.com>
2016-07-12 23:24:07 +0100

    TESTES DEBUG START CONVERSATION

--- forms/testes.php
+++ forms/testes.php
@@ -21,13 +21,13 @@ include_once ('../db/conn.inc.php');
         echo '<br>Check Sub Cat > 1 <br>';
         $subCatId = 1;
         $subCat = DB_checkSubCategoryOwner($pdo, $orgId, $subCatId);
-        if(isset($subCat)) {
+        if (isset($subCat)) {
             echo 'TEM VALOR';
         } else {
             echo 'ESTA A NULL <br>';
             var_dump($subCat);
         }
-       
+
         $catId = 1;
         echo '<br>Check Cat > 1 <br>';
         $cat = DB_checkCategoryOwner($pdo, $orgId, $catId);
@@ -39,6 +39,11 @@ include_once ('../db/conn.inc.php');
         $userId = $_SESSION['id'];
         $x = DB_getUserToStartChat($pdo, $orgServId, $userId);
         var_dump($x);
+        echo '<br>ADDING CONVERSATION<br>';
+        $d = getDateToDB();
+        $orgServ = 'TESTE';
+        $userClient = 36;
+        var_dump(DB_addConversation($pdo, $userClient, $userOrg, $d, $orgServ));
         ?>
 
     </body>
