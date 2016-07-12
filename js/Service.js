a040dd566a603b3980d2025b7681cb1c0fef7690
prcunha <prcunha.383@gmail.com>
2016-07-12 22:01:16 +0100

    SERVICE WIC

--- js/Service.js
+++ js/Service.js
@@ -48,7 +48,7 @@ function addToConversation(a, c) {
                 alert(result);
             },
             error: function (result) {
-                alert("fail");
+                alert("ERROR STARTING CONVERSATION" . result);
             }
 
         });
