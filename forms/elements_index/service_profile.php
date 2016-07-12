4b99214e7559ad1bb43ab797fcb0ffb9ef4f9fa3
prcunha <prcunha.383@gmail.com>
2016-07-12 01:05:56 +0100

    SERVICE WIC

--- forms/elements_index/service_profile.php
+++ forms/elements_index/service_profile.php
@@ -107,11 +107,11 @@ $msg;
                                         <!--<div class = "status-upload"> -->
                                         <div class = "cd-label">
                                             <!--<form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">-->
-                                                <div><h4> <?php echo $msg; ?></h4>
-                                                    <textarea id="userComment" name ="userComment" placeholder="Write your comment here" style="width: 100%"></textarea>
-                                                </div>
-                                                <!--<button type="submit" class="btn" id = "addComment" name="addComment"><i class="fa fa-reply"></i>POST </button>-->
-                                                <button onclick="addCommentToService(<?= $_SESSION['id'] ?>, <?= $orgServId ?>, <?= getDateToDB(); ?>)" class="btn" id = "addComment" name="addComment"><i class="fa fa-reply"></i>POST </button>
+                                            <div><h4> <?php echo $msg; ?></h4>
+                                                <textarea id="userComment" name ="userComment" placeholder="Write your comment here" style="width: 100%"></textarea>
+                                            </div>
+                                            <!--<button type="submit" class="btn" id = "addComment" name="addComment"><i class="fa fa-reply"></i>POST </button>-->
+                                            <button onclick="addCommentToService(<?= $_SESSION['id'] ?> , <?= $orgServId ?> , <?= getDateToDB(); ?>);" class="btn" id = "addComment" name="addComment"><i class="fa fa-reply"></i>POST </button>
                                         </div><!-- Status Upload class="btn btn-success "
                                         </div><!-- Widget Area -->
                                     </div>
