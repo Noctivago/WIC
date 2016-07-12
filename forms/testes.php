7fca1f03c84b6e3e5b6e255ef611aecca8a1e9fe
prcunha <prcunha.383@gmail.com>
2016-07-12 22:30:58 +0100

    TESTES DEBUG START CONVERSATION

--- forms/testes.php
+++ forms/testes.php
@@ -14,8 +14,13 @@ include_once ('../db/conn.inc.php');
         <?php
         echo 'Welcome ' . $_SESSION['username'] . '<br>';
         $orgServId = 2;
+        echo 'GET IDs <br>';
         $orgUsers = DB_checkUserToStartChat($pdo, $orgServId);
         var_dump($orgUsers);
+        echo 'Check Sub Cat > 1 <br>';
+        $subCatId = 1;
+        $subCat = DB_checkSubCategoryOwner($pdo, $orgId, $subCatId);
+        var_dump($subCat);
         ?>
 
     </body>
