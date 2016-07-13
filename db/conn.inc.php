f7297a8dcf1b9956cd1de0f673ec69ddbb9ee30b
prcunha <prcunha.383@gmail.com>
2016-07-14 00:16:25 +0100

    GOING TO CHAT!

--- db/conn.inc.php
+++ db/conn.inc.php
@@ -1184,7 +1184,7 @@ function DB_getMyConversations($pdo, $userId) {
     foreach ($rows as $row) {
         if ($row['UID'] != $userId) {
             echo '<div class="media conversation">';
-            echo '<a class="pull-left" href="#">';
+            echo '<a class="" href="#">';
             echo '<div>';
             #echo '<a class="pull-left">';
             echo '<img class="media-object" alt="64x64" style="width: 50px; height: 50px;" src="' . $row['PP'] . '">';
