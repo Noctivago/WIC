3f9548aebaba9c47f4ede4d96a9cb04f48459dac
PC <PC@172.20.201.47>
2016-07-14 11:23:56 +0100

    READ NOT RESPONDED

--- ajax/add_msg.php
+++ ajax/add_msg.php
@@ -6,7 +6,7 @@ require_once '../db/functions.php';
 
 //ADD MESSAGES
 //SEND MESSAGE ()
+$message = (filter_var($_POST ['msg']));
 $userId = 36;
-$message = 'QUEIMADELA!';
 $Conversation_Id = 82;
 DB_sendMessage($pdo, $userId, $message, $Conversation_Id);
