0a544bf5add2b374247deb21a81e875d048962a6
PC <PC@172.20.201.47>
2016-07-14 16:21:29 +0100

    CHAT MODE ON <- STARTING GET OF DINAMIC VALUES

--- js/OrganizationUsers.js
+++ js/OrganizationUsers.js
@@ -102,6 +102,7 @@ function change_tableData(json_r, div_table, id_table, tbody) {
             btn.type = 'button';
             btn.className = 'btn';
             btn.value = 'Remove';
+            btn.id = json_r[i].Id;
             btn.addEventListener("click", removeCategoryOwner);
         } else {
             var dd = document.createElement('input');
