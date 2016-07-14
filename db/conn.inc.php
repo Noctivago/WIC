52f031399d96d7c0381f21a39f8fcf3d201e83c6
prcunha <prcunha.383@gmail.com>
2016-07-14 01:04:41 +0100

    GOING TO CHAT!

--- db/conn.inc.php
+++ db/conn.inc.php
@@ -982,7 +982,7 @@ function DB_getCommentsOfService($pdo, $orgServId) {
             echo '<small class="pull-right time"><i class="fa fa-clock-o"></i>' . ' ' . $subStr[0] . ' ' . $h[0] . '</small>';
             echo '<h5 class="media-heading">' . $row['UU'] . '</h5>';
 #echo '<small class="col-lg-10">' . $row['CC'] . '</small>';
-            echo '<small>' . $row['CC'] . '</small>';
+            echo '<small class="col-lg-10">' . $row['CC'] . '</small>';
             echo '</div>';
             echo '</div>';
         }
@@ -1204,11 +1204,11 @@ function DB_getMyMessages($pdo, $Conversation_Id) {
     try {
         $rows = sql($pdo, "SELECT [Message].[Id]
       ,[Message].[User_Id]
-      ,[Message].[Message]
-      ,[Message].[Message_Date]
+      ,[Message].[Message] MMM
+      ,[Message].[Message_Date] AS MMD
       ,[Message].[Conversation_Id]
-	  ,[User].[Username]
-	  ,[Profile].[Picture_Path]
+	  ,[User].[Username] UUN
+	  ,[Profile].[Picture_Path] AS PP
     FROM [dbo].[Message]
     join [Conversation]
     on [Conversation].[Id] = [Message].[Conversation_Id]
@@ -1218,14 +1218,17 @@ function DB_getMyMessages($pdo, $Conversation_Id) {
     on [User].[Id] = [Message].[User_Id]
     WHERE [Conversation].[Id] = ? ORDER BY [Message].[Message_Date] DESC", array($Conversation_Id), "rows");
         foreach ($rows as $row) {
+            $str = $row['CDC'];
+            $subStr = explode(" ", $str);
+            $h = explode(".", $subStr[1]);
             echo '<div class="media msg">';
             echo '<a class="pull-left" href="#">';
-            echo '<img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACqUlEQVR4Xu2Y60tiURTFl48STFJMwkQjUTDtixq+Av93P6iBJFTgg1JL8QWBGT4QfDX7gDIyNE3nEBO6D0Rh9+5z9rprr19dTa/XW2KHl4YFYAfwCHAG7HAGgkOQKcAUYAowBZgCO6wAY5AxyBhkDDIGdxgC/M8QY5AxyBhkDDIGGYM7rIAyBgeDAYrFIkajEYxGIwKBAA4PDzckpd+322243W54PJ5P5f6Omh9tqiTAfD5HNpuFVqvFyckJms0m9vf3EY/H1/u9vb0hn89jsVj8kwDfUfNviisJ8PLygru7O4TDYVgsFtDh9Xo9NBrNes9cLgeTybThgKenJ1SrVXGf1WoVDup2u4jFYhiPx1I1P7XVBxcoCVCr1UBfTqcTrVYLe3t7OD8/x/HxsdiOPqNGo9Eo0un02gHkBhJmuVzC7/fj5uYGXq8XZ2dnop5Mzf8iwMPDAxqNBmw2GxwOBx4fHzGdTpFMJkVzNB7UGAmSSqU2RoDmnETQ6XQiOyKRiHCOSk0ZEZQcUKlU8Pz8LA5vNptRr9eFCJQBFHq//szG5eWlGA1ywOnpqQhBapoWPfl+vw+fzweXyyU+U635VRGUBOh0OigUCggGg8IFK/teXV3h/v4ew+Hwj/OQU4gUq/w4ODgQrkkkEmKEVGp+tXm6XkkAOngmk4HBYBAjQA6gEKRmyOL05GnR99vbW9jtdjEGdP319bUIR8oA+pnG5OLiQoghU5OElFlKAtCGr6+vKJfLmEwm64aosd/XbDbbyIBSqSSeNKU+HXzlnFAohKOjI6maMs0rO0B20590n7IDflIzMmdhAfiNEL8R4jdC/EZIJj235R6mAFOAKcAUYApsS6LL9MEUYAowBZgCTAGZ9NyWe5gCTAGmAFOAKbAtiS7TB1Ng1ynwDkxRe58vH3FfAAAAAElFTkSuQmCC">';
+            echo '<img class="media-object" alt="64x64" style="width: 32px; height: 32px;" src="' . $row['PP'] . '">';
             echo '</a>';
             echo '<div class="media-body">';
-            echo '<small class="pull-right time"><i class="fa fa-clock-o"></i> 12:10am</small>';
-            echo '<h5 class="media-heading">Naimish Sakhpara</h5>';
-            echo '<small class="col-lg-10">Arnab Goswami: "Some people close to Congress Party and close to the government had a #secret #meeting in a farmhouse in Maharashtra in which Anna Hazare send some representatives and they had a meeting in the discussed how to go about this all fast and how eventually this will end."</small>';
+            echo '<small class="pull-right time"><i class="fa fa-clock-o"></i>' . $subStr[0] . ' ' . $h[0] . '</small>';
+            echo '<h5 class="media-heading">' . $row['UUN'] . '</h5>';
+            echo '<small class="col-lg-10">' . $row['MMM'] . '</small>';
             echo '</div>';
             echo '</div>';
         }
