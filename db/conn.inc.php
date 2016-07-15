e3725829cce5917b70ca1518a714ae6b850f0783
prcunha <prcunha.383@gmail.com>
2016-07-15 01:02:16 +0100

    GOING TO CHAT!

--- db/conn.inc.php
+++ db/conn.inc.php
@@ -1204,7 +1204,7 @@ function DB_getMyConversations($pdo, $userId) {
             echo '</div>';
             echo '<div class="media-body">';
             echo '<h5 class="media-heading">' . $row['UUN'] . '</h5>';
-            echo '<h5 class="media-heading" id="CID">' . ' > ' . $row['CID'] . '</h5>';
+            echo '<h5 class="media-heading" id="' . $row['CID'] . '">' . ' > ' . $row['CID'] . '</h5>';
             //echo '<small>Hello</small>';
             echo '</div>';
             //echo '</a>';
