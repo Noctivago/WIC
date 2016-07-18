95e63790d9ac138ca372cc93a4197848548c7f0b
prcunha <prcunha.383@gmail.com>
2016-07-18 21:12:11 +0100

    GOING TO CHAT!

---
+++ build/db/dbconn.php
@@ -0,0 +1,44 @@
+<?php
+
+#include_once './functions.php';
+
+$dbserver = "wicsqlserver.database.windows.net";
+$dbport = "1433";
+$dbpass = '#$youcandoit2017$#';
+$dbuser = "wic";
+$db = "wic";
+
+try {
+    $pdo = new PDO("sqlsrv:server=$dbserver; Database=$db", "$dbuser", "$dbpass");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    if ($pdo == false) {
+        echo "Redirect to 404!";
+    } else {
+#echo "Connected!";
+    }
+} catch (Exception $e) {
+    echo("Error -> IP NOT ALLOWED!");
+}
+
+// Simple function to handle PDO prepared statements
+function sql($pdo, $q, $params, $return) {
+
+    try {
+
+        // Prepare statement
+        $stmt = $pdo->prepare($q);
+        // Execute statement
+        $stmt->execute($params);
+        // Decide whether to return the rows themselves, or just count the rows
+        if ($return == "rows") {
+            return $stmt->fetchAll();
+        } elseif ($return == "row") {
+            return $stmt->featch();
+        } elseif ($return == "count") {
+            return $stmt->rowCount();
+        }
+    } catch (Exception $exc) {
+        return '';
+    }
+}
