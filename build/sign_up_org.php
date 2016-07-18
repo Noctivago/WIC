69df114ce2115d77d852cb77efcf00b8de6fc1e2
prcunha <prcunha.383@gmail.com>
2016-07-18 22:44:49 +0100

    SIGN UP USER

--- build/sign_up_org.php
+++ build/sign_up_org.php
@@ -28,18 +28,17 @@ include_once '../build/db/locations.php';
                                 <div >
                                     //<?= DB_getCountryAsSelect($pdo); ?>
                                     <select id = "countrySelect" class="bootstrap-select bootstrap-select-arrow" placeholder="Country">
-                                        <option value="0">Please select</option>
-                                        
+                                        <option value='0'>Please select</option>
                                     </select>
                                 </div>
                                 <div >
                                     <select id = "stateSelect"class="bootstrap-select bootstrap-select-arrow states" placeholder="State" disabled>
-                                        <option value="0">Please select</option>
+                                        <option value='0'>Please select</option>
                                     </select>
                                 </div>
                                 <div >
                                     <select id = "citySelect" class="bootstrap-select bootstrap-select-arrow cities" placeholder="City" disabled>
-                                        <option value="0">Please select</option>
+                                        <option value='0'>Please select</option>
                                     </select>
                                 </div>
                             </div><!--.row-->
