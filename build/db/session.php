54da2c842974f582d148682cd7f4ce4ab6e6c291
prcunha <prcunha.383@gmail.com>
2016-07-20 00:42:07 +0100

    USER PROF

--- build/db/session.php
+++ build/db/session.php
@@ -4,20 +4,21 @@ include_once './dbconn.php';
 session_start(); // Starting Session
 //error_reporting(E_ALL);
 //ini_set("display_errors", 1);
-if (!isset($_SESSION['email'])) {
+if (!isset($_SESSION['id'])) {
     header('Location:../build/sign_in.php'); // Redirecting To Home Page
-}
+} else {
 
-$sId = $_SESSION['id'];
-$sEmail = $_SESSION['email'];
-$s_pw = $_SESSION['password'];
-$s_role = $_SESSION['role'];
+    $sId = $_SESSION['id'];
+    $sEmail = $_SESSION['email'];
+    $s_pw = $_SESSION['password'];
+    $s_role = $_SESSION['role'];
 
 //SE TUDO OK C/ A SESSION
-if (DB_validateUserSession($pdo, $sId, $sEmail, $s_pw, $s_role)) {
+    if (DB_validateUserSession($pdo, $sId, $sEmail, $s_pw, $s_role)) {
 
 //SENAO    
-} else {
-    session_destroy();
-    header('Location:../build/sign_in.php'); // Redirecting To Home Page
+    } else {
+        session_destroy();
+        header('Location:../build/sign_in.php'); // Redirecting To Home Page
+    }
 }
