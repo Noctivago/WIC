8b37ebfb311f0f3107b7ad9f70740b923226d9e1
prcunha <prcunha.383@gmail.com>
2016-07-19 23:00:25 +0100

    SIGN UP ORG

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -594,7 +594,7 @@ function DB_addOrgProfile($pdo, $email, $name, $city) {
  */
 function DB_validateSession($pdo, $sId, $sEmail, $s_pw) {
     try {
-        $count = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Id] = ?, [Email] = ?, "
+        $count = sql($pdo, "SELECT * FROM [dbo].[User] WHERE [Id] = ?, [Email] = ? AND "
                 . "[Password] = ?", array($sId, $sEmail, $s_pw), "count");
         if ($count < 0) {
             return true;
