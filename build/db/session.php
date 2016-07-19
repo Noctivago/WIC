71157817c20f28d08f04205b1b839a7b35119a23
prcunha <prcunha.383@gmail.com>
2016-07-20 00:53:58 +0100

    USER PROF

--- build/db/session.php
+++ build/db/session.php
@@ -2,21 +2,21 @@
 
 include_once './dbconn.php';
 session_start(); // Starting Session
-//error_reporting(E_ALL);
-//ini_set("display_errors", 1);
+//VERIFICA SE SESSION NAO EXISTE
 if (!isset($_SESSION['id'])) {
     header('Location:../build/sign_in.php'); // Redirecting To Home Page
+    //SE EXISTIR
 } else {
-
+    //FAZ GET DOS VALORES
     $sId = $_SESSION['id'];
     $sEmail = $_SESSION['email'];
     $s_pw = $_SESSION['password'];
     $s_role = $_SESSION['role'];
 
-//SE TUDO OK C/ A SESSION
+    //VERIFICA SE SESSION E TODOS OS SEUS DADOS SAO VALIDOS
     if (DB_validateUserSession($pdo, $sId, $sEmail, $s_pw, $s_role)) {
 
-//SENAO    
+        //SENAO NAO FOREM VALIDOS
     } else {
         session_unset();
         session_destroy();
