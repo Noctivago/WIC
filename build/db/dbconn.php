0878ab190207e440e6c541de1b9555cc8a05737f
prcunha <prcunha.383@gmail.com>
2016-07-19 01:52:10 +0100

    SIGN UP USER

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -245,3 +245,33 @@ function DB_resetPassword($pdo, $email) {
         return false;
     }
 }
+
+//EMAIL SEND
+function sendEmail($to, $subject, $body) {
+#error_reporting(E_STRICT);
+#configura o fuso horario
+    date_default_timezone_set('Europe/Lisbon');
+    #faz os includes necessarios das bibliotecas
+    require_once('../maiSender/class.phpmailer.php');
+    #cria uma nova instancia do PHPMailer
+    $mail = new PHPMailer();
+    $mail->IsSMTP(); // telling the class to use SMTP
+    #$mail->SMTPDebug = 2;                     // enables SMTP debug information (for testing)
+    $mail->SMTPAuth = true;                  // enable SMTP authentication
+    $mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
+    $mail->Host = "iberweb4a.ibername.com";      // sets GMAIL as the SMTP server
+    $mail->Port = 465;                   // set the SMTP port for the GMAIL server
+    $mail->Username = "donotreply@wic.club";  // GMAIL username
+    $mail->Password = '#$youcandoit2017$#';            // GMAIL password
+    $mail->SetFrom('donotreply@wic.club', 'WIC #Please do not reply!');
+    $mail->AddReplyTo("donotreply@wic.club", "WIC");
+    $mail->Subject = $subject;
+    #$mail->AltBody = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+    $mail->MsgHTML($body);
+    $mail->AddAddress($to);
+    if (!$mail->Send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        echo "Message sent!";
+    }
+}
