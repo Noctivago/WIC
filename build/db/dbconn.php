e70ab6296cbcd74f7761ee3652e56f50b1f67400
prcunha <prcunha.383@gmail.com>
2016-07-19 23:04:38 +0100

    SIGN UP ORG

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -619,7 +619,7 @@ function DB_validateUserSession($pdo, $sId, $sEmail, $s_pw, $s_role) {
     try {
         $role = DB_getUserRole($pdo, $sEmail);
         if (DB_validateSession($pdo, $sId, $sEmail, $s_pw)) {
-            if ($role === $s_role) {
+            if ($role == $s_role) {
                 return true;
             } else {
                 return false;
