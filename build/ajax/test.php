566e4151656ad5eb97605cf17476ac6308da18b5
PC <PC@172.20.201.47>
2016-07-19 12:42:35 +0100

    LOGIN

--- build/ajax/test.php
+++ build/ajax/test.php
@@ -3,6 +3,9 @@
 include_once '../db/dbconn.php';
 
 //DB_activateUserAccount($pdo, "prcunha.383@gmail.com");
+//$email = "paulo.cunha@esprominho.pt";
 $email = "paulo.cunha@esprominho.pt";
 DB_getUsersTable($pdo);
 echo '<br>' . DB_getRoleName($pdo, $email);
+
+echo 'FUCK';
