4fc4684fc57498813d3aab4bcb03b10ccdf319be
prcunha <prcunha.383@gmail.com>
2016-07-20 20:24:18 +0100

    USER PROF < GET WIC PLANNER

--- build/account_confirmation.php
+++ build/account_confirmation.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <?php
-    include("includes/head_singleforms.php");
-    
+include("includes/head_singleforms.php");
 ?>
 <body>
 
@@ -14,20 +13,20 @@
                     </div>
                     <header class="sign-title">Confirm Your Account</header>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="E-Mail or Phone"/>
+                        <input type="text" class="form-control" placeholder="E-Mail" required/>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Validation Code"/>
+                        <input type="text" class="form-control" placeholder="Validation Code" required/>
                     </div>
-<!--                    <div class="form-group">
-                        <div class="checkbox float-left">
-                            <input type="checkbox" id="signed-in"/>
-                            <label for="signed-in">Keep me signed in</label>
-                        </div>
-                        <div class="float-right reset">
-                            <a href="reset-password.html">Reset Password</a>
-                        </div>
-                    </div>-->
+                    <!--                    <div class="form-group">
+                                            <div class="checkbox float-left">
+                                                <input type="checkbox" id="signed-in"/>
+                                                <label for="signed-in">Keep me signed in</label>
+                                            </div>
+                                            <div class="float-right reset">
+                                                <a href="reset-password.html">Reset Password</a>
+                                            </div>
+                                        </div>-->
                     <button type="submit" class="btn btn-rounded">Validate account</button>
                     <p class="sign-note">New to our website? <a href="sign_up.php">Sign up</a></p>
                     <!--<button type="button" class="close">
@@ -38,10 +37,10 @@
         </div>
     </div><!--.page-center-->
 
-<script src="js/lib/jquery/jquery.min.js"></script>
-<script src="js/lib/tether/tether.min.js"></script>
-<script src="js/lib/bootstrap/bootstrap.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/app.js"></script>
+    <script src="js/lib/jquery/jquery.min.js"></script>
+    <script src="js/lib/tether/tether.min.js"></script>
+    <script src="js/lib/bootstrap/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
