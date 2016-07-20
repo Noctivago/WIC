3b4c0b051f60e4cb6ac40ddf1d056861f7b35faf
prcunha <prcunha.383@gmail.com>
2016-07-20 19:05:23 +0100

    USER PROF < GET WIC PLANNER

--- build/ajax/test.php
+++ build/ajax/test.php
@@ -4,4 +4,5 @@ include_once '../db/dbconn.php';
 
 //DB_activateUserAccount($pdo, "prcunha.383@gmail.com");
 //$email = "paulo.cunha@esprominho.pt";
-print_r($_SESSION);
+print 'SESSION <br>';
+var_dump($_SESSION);
