4c938b42a81bf9583e17894e154b7c8ce2425711
prcunha <prcunha.383@gmail.com>
2016-07-20 20:42:41 +0100

    USER PROF < GET WIC PLANNER

--- build/account_confirmation.php
+++ build/account_confirmation.php
@@ -5,35 +5,37 @@ include("../build/db/dbconn.php");
 ?>
 <body>
     <?php
-    $code = (filter_var($_POST ['activateText'], FILTER_SANITIZE_STRING));
-    $email = (filter_var($_POST ['email'], FILTER_SANITIZE_EMAIL));
-    //SE EMAIL EXISTE
-    if (DB_checkIfUserExists($pdo, $email)) {
-        //VERIFICA SE O ACTIVATION CODE PERTENCE AO EMAIL
-        if (DB_compareActivationCode($pdo, $email, $code)) {
-            //SE TRUE ATIVA CONTA
-            if (DB_activateUserAccount($pdo, $email)) {
-                $msg = 'ACCOUNT SUCESSUFULLY ACTIVATED';
-                $to = $email;
-                $subject = "WIC #ACCOUNT ACTIVATED";
-                $body = "Hi! <br>"
-                        . "Your account was successfully activated!<br>"
-                        . "You can now login and make the best event for you!<br>"
-                        . "Best regards,<br>"
-                        . "WIC<br><br>"
-                        . "Note: Please do not reply to this email! Thanks!";
-                $msg = sendEmail($to, $subject, $body) . ' Account successfully activated!';
+    if (isset($_POST['activate']) && !empty($_POST['activateText']) && !empty($_POST['email'])) {
+        $code = (filter_var($_POST ['activateText'], FILTER_SANITIZE_STRING));
+        $email = (filter_var($_POST ['email'], FILTER_SANITIZE_EMAIL));
+        //SE EMAIL EXISTE
+        if (DB_checkIfUserExists($pdo, $email)) {
+            //VERIFICA SE O ACTIVATION CODE PERTENCE AO EMAIL
+            if (DB_compareActivationCode($pdo, $email, $code)) {
+                //SE TRUE ATIVA CONTA
+                if (DB_activateUserAccount($pdo, $email)) {
+                    $msg = 'ACCOUNT SUCESSUFULLY ACTIVATED';
+                    $to = $email;
+                    $subject = "WIC #ACCOUNT ACTIVATED";
+                    $body = "Hi! <br>"
+                            . "Your account was successfully activated!<br>"
+                            . "You can now login and make the best event for you!<br>"
+                            . "Best regards,<br>"
+                            . "WIC<br><br>"
+                            . "Note: Please do not reply to this email! Thanks!";
+                    $msg = sendEmail($to, $subject, $body) . ' Account successfully activated!';
+                } else {
+                    //SENAO
+                    $msg = 'AN ERROR OCCURED WHILE ACTIVATING ACCOUNT';
+                }
             } else {
-                //SENAO
-                $msg = 'AN ERROR OCCURED WHILE ACTIVATING ACCOUNT';
+                //SENAO INFORMA
+                $msg = 'WRONG ACTIVATION CODE!';
             }
         } else {
             //SENAO INFORMA
-            $msg = 'WRONG ACTIVATION CODE!';
+            $msg = 'INCORRECT DATA. PLEASE TRY AGAIN!';
         }
-    } else {
-        //SENAO INFORMA
-        $msg = 'INCORRECT DATA. PLEASE TRY AGAIN!';
     }
     ?>
     <div class="page-center">
@@ -50,7 +52,7 @@ include("../build/db/dbconn.php");
                     <div class="form-group">
                         <input type="text" name="activateText" id="activateText" class="form-control" placeholder="Validation Code" required/>
                         <p class="sign-note">  <?= $msg; ?> </p>
-                        <button type="submit" class="btn btn-rounded">Validate account</button>
+                        <button type="submit" name="activate" id="activate" class="btn btn-rounded">Validate account</button>
                         <p class="sign-note">New to our website? <a href="sign_up.php">Sign up</a></p>
                 </form>
             </div>
