002a433bced54967099f2b3b6b1c9683ec3f5622
prcunha <prcunha.383@gmail.com>
2016-07-21 22:25:56 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/includes/showWicServices.php
+++ build/includes/showWicServices.php
@@ -1,5 +1,6 @@
 <?php
 
+include_once '../db/session.php';
 //FUNCA X) NA QUERY VERIFICAR SE O USER PERTENCE A ESTE WIC PLANNER; SE SIM MOSTRA
 //$wicPlannerId = INPUT_GET($_GET["id"]);
 $wicPlannerId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
