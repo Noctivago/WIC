2a7a21b038c90245e7f53055c382ccbd903430a8
prcunha <prcunha.383@gmail.com>
2016-07-21 01:38:39 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/includes/addWicForm.php
+++ build/includes/addWicForm.php
@@ -5,6 +5,7 @@
  * and open the template in the editor.
  */
 ?>
+<link rel="stylesheet" href="css/lib/clockpicker/bootstrap-clockpicker.min.css">
 <form class="sign-box" action="" method="post">
     <div class="sign-avatar no-photo">&plus;</div>
     <header class="sign-title">Add new Wic Planner</header>
@@ -44,3 +45,72 @@
     <p class="sign-note">  <?= $msg; ?> </p>
     <button type="submit" name="signup" class="btn btn-rounded btn-success sign-up">Add Wic Planner</button>
 </form>
+
+<script src="js/lib/jquery/jquery.min.js"></script>
+<script src="js/lib/tether/tether.min.js"></script>
+<script src="js/lib/bootstrap/bootstrap.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/app.js"></script>
+
+<script src="js/lib/clockpicker/bootstrap-clockpicker.min.js"></script>
+<script src="js/lib/clockpicker/bootstrap-clockpicker-init.js"></script>
+<script src="js/lib/daterangepicker/daterangepicker.js"></script>
+<script src="js/lib/bootstrap-select/bootstrap-select.min.js"></script>
+
+
+<script src="js/lib/jquery-tag-editor/jquery.caret.min.js"></script>
+<script src="js/lib/jquery-tag-editor/jquery.tag-editor.min.js"></script>
+<script src="js/lib/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="js/lib/select2/select2.full.min.js"></script>
+
+<script>
+    $(function () {
+        $('#tags-editor-textarea').tagEditor();
+    });
+</script>
+
+
+
+
+<!-- RECHAPTA -->
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<script>
+    $(function () {
+        function cb(start, end) {
+            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+        cb(moment().subtract(29, 'days'), moment());
+
+        $('#daterange').daterangepicker({
+            "timePicker": true,
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
+            "linkedCalendars": false,
+            "autoUpdateInput": false,
+            "alwaysShowCalendars": true,
+            "showWeekNumbers": true,
+            "showDropdowns": true,
+            "showISOWeekNumbers": true
+        });
+
+        $('#daterange2').daterangepicker();
+
+        $('#daterange3').daterangepicker({
+            singleDatePicker: true,
+            showDropdowns: true
+        });
+
+        $('#daterange').on('show.daterangepicker', function (ev, picker) {
+            /*$('.daterangepicker select').selectpicker({
+             size: 10
+             });*/
+        });
+    });
+</script>
