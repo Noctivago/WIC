135a17e8fb7c404614b7e65e8666483ea6e48821
prcunha <prcunha.383@gmail.com>
2016-07-21 01:33:33 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/my_wicplanner.php
+++ build/my_wicplanner.php
@@ -76,45 +76,8 @@ $msg = '';
 
             <!--A PUTA COMEÇA AQUI-->
 
-            <div class="col-lg-6">
-                <!--<div class="col-lg-3 col-lg-pull-6 col-md-6 col-sm-6">-->
-                <section class="box-typical box-typical-max-280">
-                    <header class="box-typical-header">
-                        <div class="tbl-row">
-                            <div class="tbl-cell tbl-cell-title">
-                                <h3> Services in "Event Name"</h3>
-                            </div>
-                        </div>
-                    </header>
-                    <div class="box-typical-body" style="overflow: hidden; padding: 0px; height: 700px; width: 504px;">
-                        <div class="table-responsive">
-                            <table class="table table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>Service</th>
-                                        <th>Organization</th><th></th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="table-check">
-                                        <td><a href="#">Nome</a></td>
-                                        <td class="table-photo">
-                                            <img src="img/photo-64-1.jpg" alt="" data-toggle="tooltip" data-placement="bottom" title="Nicholas<br/>Barrett">
-                                        </td>
-                                        <td class="table-photo">
-                                        </td>
-                                        <td class="table-photo">
-                                            <a href="#" class="font-icon font-icon-del">
-                                            </a>
-                                            <!--<span>&cross;</span></a>-->
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div><!--.box-typical-body-->
-                </section><!--.box-typical-->
+            <div id = "include" class="col-lg-6">
+
             </div>
 
         </div><!--.container-fluid-->
