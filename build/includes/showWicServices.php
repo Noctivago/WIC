1444494cd2ff01235702f1775f4dfb8b5722d5f0
prcunha <prcunha.383@gmail.com>
2016-07-21 22:38:01 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/includes/showWicServices.php
+++ build/includes/showWicServices.php
@@ -2,8 +2,9 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-include_once './db/session.php';
-include_once './db/dbconn.php';
+include_once '../../build/db/session.php';
+include_once '../../build/db/dbconn.php';
+
 //FUNCA X) NA QUERY VERIFICAR SE O USER PERTENCE A ESTE WIC PLANNER; SE SIM MOSTRA
 //$wicPlannerId = INPUT_GET($_GET["id"]);
 $wicPlannerId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
