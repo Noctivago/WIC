1cde706c84f03ea41d25153bce8ae858b9c6c96c
PC <PC@172.20.201.47>
2016-07-21 10:22:16 +0100

    WICPLANNER < WORKING ON INCLUDES

--- build/includes/showWicServices.php
+++ build/includes/showWicServices.php
@@ -1,6 +1,7 @@
 <?php
 //FUNCA X) NA QUERY VERIFICAR SE O USER PERTENCE A ESTE WIC PLANNER; SE SIM MOSTRA
-$wicPlannerId = INPUT_GET($_GET["id"]);
+//$wicPlannerId = INPUT_GET($_GET["id"]);
+$wicPlannerId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 //$UserId = $_GET["uId"];
 var_dump($wicPlannerId);
 ?>
