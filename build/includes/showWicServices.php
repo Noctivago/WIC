2ae5d2d8657745a2816a75d1a711ea2da2be6627
prcunha <prcunha.383@gmail.com>
2016-07-21 22:16:51 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/includes/showWicServices.php
+++ build/includes/showWicServices.php
@@ -1,8 +1,9 @@
 <?php
+
 //FUNCA X) NA QUERY VERIFICAR SE O USER PERTENCE A ESTE WIC PLANNER; SE SIM MOSTRA
 //$wicPlannerId = INPUT_GET($_GET["id"]);
 $wicPlannerId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-$UserId = $_GET["uId"];
+$UserId = $_SESSION['id'];
 //var_dump($wicPlannerId);
 echo db_getServicesOfMyWicPlanner($pdo, $wicPlannerId, $userId);
 
