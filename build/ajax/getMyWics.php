f459f92a8d34536dd31673b68007e7f56282cdc3
prcunha <prcunha.383@gmail.com>
2016-07-21 23:39:49 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/ajax/getMyWics.php
+++ build/ajax/getMyWics.php
@@ -3,6 +3,7 @@
 include_once '../db/session.php';
 require_once '../db/dbconn.php';
 require_once '../db/functions.php';
-
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 db_getMyWicPlannerToWICCrud($pdo, $_SESSION['id']);
 db_getThirdWicPlannerToWICCrud($pdo, $_SESSION['id']);
