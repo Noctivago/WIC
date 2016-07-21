1a37c279a26e8c76d8f80983d7567710ac3e72a2
prcunha <prcunha.383@gmail.com>
2016-07-21 23:56:51 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/ajax/getMyWics.php
+++ build/ajax/getMyWics.php
@@ -1,8 +1,8 @@
 <?php
 
 include_once '../db/session.php';
-include_once '../db/dbconn.php';
-include_once '../db/functions.php';
+//include_once '../db/dbconn.php';
+//include_once '../db/functions.php';
 //error_reporting(E_ALL);
 //ini_set("display_errors", 1);
 $userId = $_SESSION['id'];
