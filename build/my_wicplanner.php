009499ac490d4496d47703ff6a1456e4a616260d
prcunha <prcunha.383@gmail.com>
2016-07-22 00:10:32 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/my_wicplanner.php
+++ build/my_wicplanner.php
@@ -8,7 +8,7 @@ ini_set("display_errors", 1);
 $msg = '';
 ?>
 
-<div class="page-content" style="height: 817px;">
+<div onload="loadMyWics()" class="page-content" style="height: 817px;">
     <div class="container-fluid">
         <div class="row" style="height: 700px;">
             <div class="col-lg-6">
