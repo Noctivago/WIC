dfc49efca7054e0716dd44254bce2dccb5857589
prcunha <prcunha.383@gmail.com>
2016-07-22 01:37:09 +0100

    Creating my_wicplanner includes -> addWicForm && showWicServices

--- build/ajax/remove_wic_planner.php
+++ build/ajax/remove_wic_planner.php
@@ -4,7 +4,7 @@ require_once '../db/dbconn.php';
 include_once '../db/session.php';
 
 $WicPlannerId = (filter_var($_POST ['con']));
-$userId = $SESSION['id'];
+$userId = $_SESSION['id'];
 
 //REMOVE WICPLANNER
 DB_removeWICPlanner($pdo, $userId, $WicPlannerId);
