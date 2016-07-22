9870c14709aa9ee5ea259d4d071158c492b61e83
PC <PC@172.20.201.47>
2016-07-22 16:47:43 +0100

    WICPLANNER CREAT OK EDIT OK REMOVE OK SEE SERVICES OK

--- build/addTowicplanner.php
+++ build/addTowicplanner.php
@@ -5,7 +5,7 @@
 <div class="container-fluid">
     <div class="sign-box">
         <div class="sign-avatar no-photo">&plus;</div>
-        <header class="sign-title">#Wic Planner</header>
+        <header class="sign-title">#You have no services in this Event!</header>
         <span id="textelement" class="form-control" style="border:0px"></span>
         <button   class="btn btn-rounded btn-success sign-up">Start Planning</button>
     </div>
