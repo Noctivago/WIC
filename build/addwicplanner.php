b829aee1e5066a59008f8a10a948f583de22d654
PC <PC@172.20.201.47>
2016-07-22 11:04:31 +0100

    WICPLANNER < WORKING ON EDIT WIC

--- build/addwicplanner.php
+++ build/addwicplanner.php
@@ -17,7 +17,7 @@ if (isset($id) && isset($uid)) {
     $wicInfo = DB_getWicPlannerInfo($pdo, $wicId, $userId);
 } else {
     $wicInfo["Name"] = 'Event Name';
-    $wicInfo["Event_Date"] = '01/08/2016';
+    $wicInfo["Event_Date"] = '2016-08-01';
 }
 ?>
 <!--<body>-->
