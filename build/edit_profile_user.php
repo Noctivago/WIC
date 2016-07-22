bd5d9c1d4d161c2a6ecfd33ea990d54c6c1e4b32
prcunha <prcunha.383@gmail.com>
2016-07-22 20:40:16 +0100

    EDIT USER PROFILE

--- build/edit_profile_user.php
+++ build/edit_profile_user.php
@@ -17,7 +17,7 @@ $msg = "";
         $lastName = (filter_var($_POST ['last'], FILTER_SANITIZE_STRING));
         $msg = DB_UpdateUserInformation($pdo, $userId, $firstName, $lastName);
 
-        $target_dir = "./img/pics/";
+        $target_dir = "pics/";
         $target_file = $target_dir . basename($_FILES["Photo"]["name"]);
         $uploadOk = 1;
         $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
