e9dd4c7693ad03fb0fc4238f9816e41b32a61296
prcunha <prcunha.383@gmail.com>
2016-07-23 02:33:41 +0100

    DISPLAY SERVIÇOS

--- build/includes/head_sideMenu.php
+++ build/includes/head_sideMenu.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-include_once '../includes/head_singleforms.php';
+//include_once '../includes/head_singleforms.php';
 include_once '../build/db/dbconn.php';
 include_once '../build/db/functions.php';
 include_once '../db/session.php';
