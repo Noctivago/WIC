c168ab12f779afddd2fdeca6b71d46dabd8ccb4b
prcunha <prcunha.383@gmail.com>
2016-07-23 02:05:15 +0100

    DISPLAY SERVIÇOS

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -1822,7 +1822,7 @@ function DB_getServicesForIndex($pdo, $Category, $SubCategoty, $city) {
                         </div>
                         <div class="card-typical-section card-typical-content">
                         <div class="photo">
-                                <img src="' . $row['MPP'] . '" alt="Service Pic"  height="180" width="180">
+                                <img src="' . $row['MPP'] . '" alt="Service Pic"  height="100" width="100">
                             </div>
                             <header class="title"><a href="service_profile.php?Service=' . $row['SID'] . '">' . $row['SNA'] . '</a></header>
                             <p>' . $row['SDE'] . '</p>
