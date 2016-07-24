1a9a5353cd5e85e3e53e55a5f781031b9200bb58
prcunha <prcunha.383@gmail.com>
2016-07-24 23:23:01 +0100

    WORKING ON ADD2WicPlanner by POPUP

--- build/index_teste.php
+++ build/index_teste.php
@@ -31,8 +31,9 @@ include ("./db/dbconn.php");
 <script src="js/lib/salvattore/salvattore.min.js"></script>
 
 <script>
-    function popupCenter(pageURL) {
-            window.open(pageURL);
+    function popupCenter() {
+        var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+        myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>
     }
 </script>
 
