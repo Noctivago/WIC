f68bcb6c52fa33c08fe34d483687d666e8ba16a2
prcunha <prcunha.383@gmail.com>
2016-07-24 03:14:02 +0100

    Service Layout ADDING COMMENT OK LOAD COMMENT AJAX OK GET ORG INFO OK

--- build/index_teste.php
+++ build/index_teste.php
@@ -11,32 +11,7 @@
                 <?= DB_getServicesForIndex($pdo, $Category, $SubCategoty, $city); ?>
             </div><!--.card-grid-->
             <div class="clear"></div>
-
-            <!--TESTE-->
-            <div data-role="page">
-                <div data-role="header">
-                    <h1>Welcome To My Homepage</h1>
-                </div>
-
-                <div data-role="main" class="ui-content">
-                    <a href="#myPopup" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all ui-icon-check ui-btn-icon-left">Show Popup Form</a>
-
-                    <div data-role="popup" id="myPopup" class="ui-content" style="min-width:250px;">
-                        <form method="post" action="demoform.asp">
-                            <div>
-                                <h3>Login information</h3>
-                                <label for="usrnm" class="ui-hidden-accessible">Username:</label>
-                                <input type="text" name="user" id="usrnm" placeholder="Username">
-                                <label for="pswd" class="ui-hidden-accessible">Password:</label>
-                                <input type="password" name="passw" id="pswd" placeholder="Password">
-                                <label for="log">Keep me logged in</label>
-                                <input type="checkbox" name="login" id="log" value="1" data-mini="true">
-                                <input type="submit" data-inline="true" value="Log in">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <!--TESTE-->
+            
             </div>
         </div>
 
