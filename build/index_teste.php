b3dd919940b8e2ba2fc007d5b834921c34dcedcc
prcunha <prcunha.383@gmail.com>
2016-07-24 02:59:20 +0100

    Service Layout ADDING COMMENT OK LOAD COMMENT AJAX OK GET ORG INFO OK

--- build/index_teste.php
+++ build/index_teste.php
@@ -15,26 +15,6 @@
         </div>
     </div>
 
-    <div id="dialog-form" title="Add to your WiC Planner">
-        <p class="validateTips">All form fields are required.</p>
-
-        <form>
-            <fieldset>
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name" value="Jane Smith" class="text ui-widget-content ui-corner-all">
-                <label for="email">Email</label>
-                <input type="text" name="email" id="email" value="jane@smith.com" class="text ui-widget-content ui-corner-all">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" value="xxxxxxx" class="text ui-widget-content ui-corner-all">
-
-                <!-- Allow form submission with keyboard without duplicating the dialog button -->
-                <input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
-            </fieldset>
-        </form>
-    </div>
-
-    <button id="create-user">Create new user</button>
-
     <script src="js/lib/jquery/jquery.min.js" type="text/javascript"></script>
     <script src="js/lib/tether/tether.min.js" type="text/javascript"></script>
 
@@ -48,98 +28,6 @@
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
     <script src="js/lib/salvattore/salvattore.min.js"></script>
-
-    <script>
-        $(function () {
-            var dialog, form,
-                    // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-                    emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-                    name = $("#name"),
-                    email = $("#email"),
-                    password = $("#password"),
-                    allFields = $([]).add(name).add(email).add(password),
-                    tips = $(".validateTips");
-
-            function updateTips(t) {
-                tips
-                        .text(t)
-                        .addClass("ui-state-highlight");
-                setTimeout(function () {
-                    tips.removeClass("ui-state-highlight", 1500);
-                }, 500);
-            }
-
-            function checkLength(o, n, min, max) {
-                if (o.val().length > max || o.val().length < min) {
-                    o.addClass("ui-state-error");
-                    updateTips("Length of " + n + " must be between " +
-                            min + " and " + max + ".");
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-            function checkRegexp(o, regexp, n) {
-                if (!(regexp.test(o.val()))) {
-                    o.addClass("ui-state-error");
-                    updateTips(n);
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-            function addUser() {
-                var valid = true;
-                allFields.removeClass("ui-state-error");
-
-                valid = valid && checkLength(name, "username", 3, 16);
-                valid = valid && checkLength(email, "email", 6, 80);
-                valid = valid && checkLength(password, "password", 5, 16);
-
-                valid = valid && checkRegexp(name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
-                valid = valid && checkRegexp(email, emailRegex, "eg. ui@jquery.com");
-                valid = valid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9");
-
-                if (valid) {
-                    $("#users tbody").append("<tr>" +
-                            "<td>" + name.val() + "</td>" +
-                            "<td>" + email.val() + "</td>" +
-                            "<td>" + password.val() + "</td>" +
-                            "</tr>");
-                    dialog.dialog("close");
-                }
-                return valid;
-            }
-
-            dialog = $("#dialog-form").dialog({
-                autoOpen: false,
-                height: 400,
-                width: 350,
-                modal: true,
-                buttons: {
-                    "Create an account": addUser,
-                    Cancel: function () {
-                        dialog.dialog("close");
-                    }
-                },
-                close: function () {
-                    form[ 0 ].reset();
-                    allFields.removeClass("ui-state-error");
-                }
-            });
-
-            form = dialog.find("form").on("submit", function (event) {
-                event.preventDefault();
-                addUser();
-            });
-
-            $("#create-user").button().on("click", function () {
-                dialog.dialog("open");
-            });
-        });
-    </script>
 
     <script>
         $(document).ready(function () {
