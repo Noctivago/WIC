242b923054f13953e5e16a10f1b345846c584aed
prcunha <prcunha.383@gmail.com>
2016-07-24 03:22:28 +0100

    Service Layout ADDING COMMENT OK LOAD COMMENT AJAX OK GET ORG INFO OK

--- build/index_teste.php
+++ build/index_teste.php
@@ -11,33 +11,6 @@ include ("./db/dbconn.php");
         </div><!--.card-grid-->
         <div class="clear"></div>
 
-
-        <div data-role="page">
-            <div data-role="header">
-                <h1>Welcome To My Homepage</h1>
-            </div>
-
-            <div data-role="main" >
-                <a href="#myPopup" data-rel="popup" >Show Popup Form</a>
-
-                <div data-role="popup" id="myPopup" style="min-width:250px;">
-                    <form method="post" action="demoform.asp">
-                        <div>
-                            <h3>Login information</h3>
-                            <label for="usrnm" >Username:</label>
-                            <input type="text" name="user" id="usrnm" placeholder="Username">
-                            <label for="pswd" >Password:</label>
-                            <input type="password" name="passw" id="pswd" placeholder="Password">
-                            <label for="log">Keep me logged in</label>
-                            <input type="checkbox" name="login" id="log" value="1" data-mini="true">
-                            <input type="submit" data-inline="true" value="Log in">
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-
-
         </div>
     </div>
 
