38a9470720ab4017a72e8eea7a65ff4683d2675a
prcunha <prcunha.383@gmail.com>
2016-07-24 23:11:08 +0100

    WORKING ON ADD2WicPlanner by POPUP

--- build/index_teste.php
+++ build/index_teste.php
@@ -32,9 +32,11 @@ include ("./db/dbconn.php");
 
 <script>
     function popupCenter(pageURL) {
-        var left = (screen.width / 2) - (800 / 2);
-        var top = (screen.height / 2) - (800 / 2);
-        var targetWin = window.open(pageURL, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+        var w = 800;
+        var h = 800;
+        var left = (screen.width / 2) - (w / 2);
+        var top = (screen.height / 2) - (h / 2);
+        window.open(pageURL, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
     }
 </script>
 
