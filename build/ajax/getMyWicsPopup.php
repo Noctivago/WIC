122c11908ac344eab81a0b11bda58681a2104152
prcunha <prcunha.383@gmail.com>
2016-07-25 00:44:32 +0100

    WORKING ON ADD2WicPlanner by POPUP

--- build/ajax/getMyWicsPopup.php
+++ build/ajax/getMyWicsPopup.php
@@ -8,7 +8,8 @@ include_once '../db/session.php';
 //var_dump($_SESSION);
 $msg = '';
 $userId = $_SESSION['id'];
-if($_SESSION['role'] !== 'user') {
+$serviceId = $_GET ['id'];
+if ($_SESSION['role'] !== 'user') {
     //header('Location: http://www.example.com/');
 }
 
@@ -33,7 +34,7 @@ if (isset($_GET['add2WiC']) && isset($_GET ['id'])) {
         <link href="../css/main.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <form class="sign-box" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get">
+        <form class="sign-box" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'] . '?id=' . $serviceId); ?>" method="get">
             <div class="sign-box">
                 <div class="sign-avatar no-photo">&plus;</div>
                 <header class="sign-title">#Add Service to WiC Planner?</header>
