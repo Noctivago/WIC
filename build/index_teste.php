88c1cdb822af8c9da8695bb4e84f371753f771ff
prcunha <prcunha.383@gmail.com>
2016-07-24 23:07:13 +0100

    WORKING ON ADD2WicPlanner by POPUP

--- build/index_teste.php
+++ build/index_teste.php
@@ -31,10 +31,10 @@ include ("./db/dbconn.php");
 <script src="js/lib/salvattore/salvattore.min.js"></script>
 
 <script>
-    function popupCenter(pageURL, w, h) {
-        var left = (screen.width / 2) - (w / 2);
-        var top = (screen.height / 2) - (h / 2);
-        window.open(pageURL, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    function popupCenter(pageURL) {
+        var left = (screen.width / 2) - (800 / 2);
+        var top = (screen.height / 2) - (800 / 2);
+        var targetWin = window.open(pageURL, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
     }
 </script>
 
