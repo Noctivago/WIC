2573394a8290d60755b8b0efb368023e8916ad35
prcunha <prcunha.383@gmail.com>
2016-07-24 01:00:44 +0100

    Service Layout ADDING COMMENT OK LOAD COMMENT AJAX OK GET ORG INFO OK

--- build/service_profile.php
+++ build/service_profile.php
@@ -46,11 +46,11 @@ $serviceId = (filter_var($_GET['Service']));
                 </section>
 
                 <section class="box-typical">
-                    <header class="box-typical-header-sm">Personal Info</header>
+                    <!--<header class="box-typical-header-sm">Personal Info</header>-->
                     <article class="profile-info-item">
                         <header class="profile-info-item-header">
                             <i class="font-icon font-icon-notebook-bird"></i>
-                            Summary
+                            Description
                         </header>
                         <div class="text-block text-block-typical">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
