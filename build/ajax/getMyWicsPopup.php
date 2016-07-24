24c47a4be406c8f6ecb0173727d18454646495de
prcunha <prcunha.383@gmail.com>
2016-07-24 23:34:10 +0100

    WORKING ON ADD2WicPlanner by POPUP

--- build/ajax/getMyWicsPopup.php
+++ build/ajax/getMyWicsPopup.php
@@ -17,23 +17,16 @@ $userId = $_SESSION['id'];
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>StartUI - Premium Bootstrap 4 Admin Dashboard Template</title>
 
-        <link href="img/wic_logo.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
-        <link href="img/wic_logo.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
-        <link href="img/wic_logo.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
-        <link href="img/wic_logo.png" rel="apple-touch-icon" type="image/png">
-        <link href="img/wic_logo.png" rel="icon" type="image/png">
-        <link href="img/wic_logo.png" rel="shortcut icon">
-
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
 
-        <link href="css/lib/lobipanel/lobipanel.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/lib/jqueryui/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/lib/font-awesome/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/main.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/lib/lobipanel/lobipanel.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/lib/jqueryui/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/lib/font-awesome/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/main.css" rel="stylesheet" type="text/css"/>
 
 
 
@@ -51,16 +44,16 @@ $userId = $_SESSION['id'];
         </div>
 
 
-        <script src="js/lib/jquery/jquery.min.js" type="text/javascript"></script>
-        <script src="js/lib/tether/tether.min.js" type="text/javascript"></script>
+        <script src="../js/lib/jquery/jquery.min.js" type="text/javascript"></script>
+        <script src="../js/lib/tether/tether.min.js" type="text/javascript"></script>
 
-        <script src="js/lib/tether/tether.min.js"></script>
-        <script src="js/lib/bootstrap/bootstrap.min.js"></script>
-        <script src="js/plugins.js"></script>
+        <script src="../js/lib/tether/tether.min.js"></script>
+        <script src="../js/lib/bootstrap/bootstrap.min.js"></script>
+        <script src="../js/plugins.js"></script>
 
-        <script type="text/javascript" src="js/lib/jqueryui/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="js/lib/lobipanel/lobipanel.min.js"></script>
-        <script type="text/javascript" src="js/lib/match-height/jquery.matchHeight.min.js"></script>
+        <script type="text/javascript" src="../js/lib/jqueryui/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="../js/lib/lobipanel/lobipanel.min.js"></script>
+        <script type="text/javascript" src="../js/lib/match-height/jquery.matchHeight.min.js"></script>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
         <script src="js/lib/salvattore/salvattore.min.js"></script>
@@ -163,12 +156,12 @@ $userId = $_SESSION['id'];
                 });
         </script>
 
-        <script src="js/app.js"></script>
+        <script src="../js/app.js"></script>
 
-        <script src="js/lib/jquery-tag-editor/jquery.caret.min.js"></script>
-        <script src="js/lib/jquery-tag-editor/jquery.tag-editor.min.js"></script>
-        <script src="js/lib/bootstrap-select/bootstrap-select.min.js"></script>
-        <script src="js/lib/select2/select2.full.min.js"></script>
+        <script src="../js/lib/jquery-tag-editor/jquery.caret.min.js"></script>
+        <script src="../js/lib/jquery-tag-editor/jquery.tag-editor.min.js"></script>
+        <script src="../js/lib/bootstrap-select/bootstrap-select.min.js"></script>
+        <script src="../js/lib/select2/select2.full.min.js"></script>
 
         <script>
                 $(function () {
