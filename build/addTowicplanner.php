4546d95b50e30f23652bf7f83eeaff331d977772
PC <PC@172.20.201.47>
2016-07-25 11:43:13 +0100

    Activation via link

--- build/addTowicplanner.php
+++ build/addTowicplanner.php
@@ -6,7 +6,7 @@
     <div class="sign-box">
         <!--<div class="sign-avatar no-photo">&plus;</div>-->
         <header class="sign-title">#No services in this Event!</header>
-        <button href="../build/index.php" class="btn btn-rounded btn-success sign-up">Start Planning</button>
+        <a href="../build/index.php" class="btn btn-rounded btn-success sign-up">Start Planning</a>
     </div>
 </div>
 
