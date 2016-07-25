6dac2101ad53c03b9271427c1d0bb7d11e3d41b1
PC <PC@172.20.201.47>
2016-07-25 13:59:54 +0100

    EDIT USER PROFILE - 100 GOING TO EDIT ORG PROF

--- build/edit_profile_org.php
+++ build/edit_profile_org.php
@@ -26,6 +26,37 @@ $msg = '';
         $twitter = (filter_var($_POST ['twitter'], FILTER_SANITIZE_STRING));
         $description = (filter_var($_POST ['description'], FILTER_SANITIZE_STRING));
         $msg = DB_UpdateOrgInformation($pdo, $name, $email, $address, $phone, $mobile, $website, $facebook, $linkdin, $twitter, $description, $userId);
+        //FALTA ACERTAR NISTO
+        $name = $_FILES['file_upload']['name'];
+        $newfilename = $userId . '.jpg';
+        if (isset($name)) {
+            if (!empty($name)) {
+                // Check for errors
+                if ($_FILES['file_upload']['error'] > 0) {
+                    die('An error ocurred when uploading.');
+                }
+                if (!getimagesize($_FILES['file_upload']['tmp_name'])) {
+                    die('Please ensure you are uploading an image.');
+                }
+                // Check filetype
+                if ($_FILES['file_upload']['type'] != 'image/png') {
+                    die('Unsupported filetype uploaded.');
+                }
+                // Check filesize
+                if ($_FILES['file_upload']['size'] > 500000) {
+                    die('File uploaded exceeds maximum upload size.');
+                }
+                
+                if (!move_uploaded_file($_FILES['file_upload']['tmp_name'], 'pics/' . $newfilename)) {
+                    die('Error uploading file - check destination is writeable.');
+                } else {
+                    $picture_path = 'pics/' . $newfilename;
+                    //filter_input(INPUT_SERVER, 'DOCUMENT_ROOT');
+                    DB_UpdateUserPictureInformation($pdo, $userId, $picture_path);
+                    $msg = ('File uploaded successfully.');
+                }
+            }
+        }
     }
     ?>
 
