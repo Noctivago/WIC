3372a8daeb05f6ffcb3c8bdef8232819b3117f34
PC <PC@172.20.201.47>
2016-07-25 13:57:53 +0100

    EDIT USER PROFILE - 100 GOING TO EDIT ORG PROF

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -1701,7 +1701,7 @@ function DB_UserProfile($pdo, $userId) {
             echo '<div class="sign-avatar no-photo" >
                         <img id="image" src="' . $row['Picture_Path'] . '" alt="Avatar"/>
                     </div>
-                    <input type="file" id="file_upload" name="file_upload">
+                   <input class="btn btn-rounded btn-file" type="file" id="file_upload" name="file_upload">
                     <header class="sign-title">Edit Profile</header>
                     <div class="form-group">
                         <div class="form-control-wrapper form-control-icon-left" >
@@ -1727,9 +1727,7 @@ function DB_OrgProfile($pdo, $userId) {
         $rows = sql($pdo, "SELECT * FROM [Organization] Where [User_Boss] = ?", array($userId), "rows");
         foreach ($rows as $row) {
             echo '<div class="sign-avatar no-photo" ><img id="image" src="' . $row['Picture_Path'] . '" alt=""/> </div>
-                    
-                    <button type="submit" class="btn btn-rounded btn-file">Change Picture <input class="btn-file" type="file"/> </button>
-                    
+                     <input class="btn btn-rounded btn-file" type="file" id="file_upload" name="file_upload">
                     <header class="sign-title">Edit Organization Profile</header>
                     <div class="form-group">
                         <div class="form-control-wrapper form-control-icon-left" >
