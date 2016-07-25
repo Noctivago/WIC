32d6575b7c83939df13235bb6270bca6838b07dc
PC <PC@172.20.201.47>
2016-07-25 12:07:38 +0100

    GOING TO PICs UPLOAD

--- build/edit_profile_org.php
+++ build/edit_profile_org.php
@@ -3,7 +3,7 @@ include ("includes/head_sideMenu.php");
 include_once '../build/db/dbconn.php';
 include_once '../build/db/session.php';
 //FUTURAMENTE COLOCAR SE USER IN SERVICE PODE EDITAR
-if ($_SESSION['role'] === 'user') {
+if ($_SESSION['role'] === "user") {
     header("location: ../build/index.php");
 }
 $msg = '';
