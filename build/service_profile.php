43de69be87fc2fdfec239099d5726bdbc8f633e7
prcunha <prcunha.383@gmail.com>
2016-07-25 03:34:25 +0100

    SERVICES INDEX SEARCH BY CATEGORY ADD SERVICE TO WIC OK BY POPUP

--- build/service_profile.php
+++ build/service_profile.php
@@ -80,7 +80,7 @@ $serviceId = (filter_var($_GET['Service']));
                         echo '    <div class="user-card-row">';
                         echo '      <div class="tbl-row">';
                         echo '        <div class="tbl-cell">';
-                        echo '            <a href="edit_profile_service.php?Service=' . $serviceId . '" class="btn btn-rounded" role="button">Edit Service</a>';
+                        echo '            <a href="edit_profile_service.php?Service=' . $serviceId . '" class="btn btn-rounded" role="button">Click to edit Service</a>';
                         echo '         </div>';
                         echo '  </div>';
                         echo ' </article>';
