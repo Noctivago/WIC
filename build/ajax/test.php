831b7832fe9b86ade51a72fdb96b0f76b8736700
PC <PC@172.20.201.47>
2016-07-26 11:43:58 +0100

    SAVING SERVICE | WITH PROFILE PICTURE PROBLEMA SUBBAT ID

--- build/ajax/test.php
+++ build/ajax/test.php
@@ -14,5 +14,6 @@ echo '<br>';
 DB_getUserProfileTable($pdo);
 echo '<br>';
 echo filter_input(INPUT_SERVER, 'DOCUMENT_ROOT');
+echo '<a href="' . filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/build/index.php">';
 echo '<br>';
 echo __DIR__;
