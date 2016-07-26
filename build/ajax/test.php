a14b6a98aff42edcdf80f7d8adfecf5bb4f66b62
PC <PC@172.20.201.47>
2016-07-26 11:47:23 +0100

    SAVING SERVICE | WITH PROFILE PICTURE PROBLEMA SUBBAT ID

--- build/ajax/test.php
+++ build/ajax/test.php
@@ -17,4 +17,4 @@ echo filter_input(INPUT_SERVER, 'DOCUMENT_ROOT');
 echo '<br>';
 echo __DIR__;
 echo '<br>';
-echo '<a href="' . filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/build/index.php">TESTE</a>';
+echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
