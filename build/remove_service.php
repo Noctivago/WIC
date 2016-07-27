1c9baa26a650643a307afd9beeef6a03d98fc071
prcunha <prcunha.383@gmail.com>
2016-07-27 20:14:25 +0100

    REMOVING SERVICE

--- build/remove_service.php
+++ build/remove_service.php
@@ -6,7 +6,7 @@ include_once '../build/db/session.php';
 $serviceId = (filter_var($_POST['Service']));
 $UserBoss = $_SESSION['id'];
 echo $UserBoss . '<br>';
-$val = DB_GetUserBossIdByService($pdo, $service_Id);
+$val = DB_GetUserBossIdByService($pdo, $serviceId);
 echo $val;
 //SE ID DO USER FOR IGUAL AO DO BOSS DA ORG ENTAO PODE APAGAR
 if ($val === $UserBoss) {
