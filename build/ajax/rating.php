20b9612e73b44d4f6d73fca2533e5134e16f7b52
PC <PC@172.20.201.47>
2016-07-27 12:20:15 +0100

    RATING SERVICE

--- build/ajax/rating.php
+++ build/ajax/rating.php
@@ -8,6 +8,5 @@ $userId = $_SESSION['id'];
 if (isset($_POST['rate']) && !empty($_POST['rate'])) {
     $rate = (filter_var($_POST ['rate']));
     $serviceId = (filter_var($_POST ['service']));
-
-    DB_addUserRateService($pdo, $userId, $serviceId, $rate);
+    echo DB_addUserRateService($pdo, $userId, $serviceId, $rate);
 }
