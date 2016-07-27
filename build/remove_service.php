988f6d2fd28de6ca8be999102c99e43686b45baa
prcunha <prcunha.383@gmail.com>
2016-07-27 20:24:38 +0100

    REMOVING SERVICE

--- build/remove_service.php
+++ build/remove_service.php
@@ -13,9 +13,9 @@ if ($val === $UserBoss) {
     //REMOVER > HEADER LOCATION PROFILE ORG
     DB_removeService($pdo, $serviceId);
     $org = DB_GetOrgIdByUserBossId($pdo, $UserBoss);
-    header("location: profile_org.php?Organization=" . $org);
+    header('location: profile_org.php?Organization=' . $org);
 } else {
     //NAO E BOSS MANDA PASSEAR
-    header("location: index.php");
+    header('location: index.php');
 }
 
