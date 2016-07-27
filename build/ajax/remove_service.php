defc36f2cb8e0e0e6b178f1e0f2dc9aebaa0002f
prcunha <prcunha.383@gmail.com>
2016-07-27 20:37:38 +0100

    REMOVING SERVICE

--- build/ajax/remove_service.php
+++ build/ajax/remove_service.php
@@ -3,16 +3,13 @@
 include_once '../build/db/dbconn.php';
 include_once '../build/db/session.php';
 
-$serviceId = (filter_var($_POST['sId']));
+$serviceId = (filter_var($_POST ['sId']));
 $UserBoss = $_SESSION['id'];
-//echo 'BOSS >' . $UserBoss . '<br>';
+
 $val = DB_GetUserBossIdByService($pdo, $serviceId);
-//echo 'KeyBD >' . $val;
+
 //SE ID DO USER FOR IGUAL AO DO BOSS DA ORG ENTAO PODE APAGAR
 if ($val === $UserBoss) {
-    //REMOVER > HEADER LOCATION PROFILE ORG
     DB_removeService($pdo, $serviceId);
-    $org = DB_GetOrgIdByUserBossId($pdo, $UserBoss);
-    //header('location: profile_org.php?Organization=' . $org);
 } 
 
