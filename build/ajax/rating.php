f6dfcbf5a90f403f7424a9e6d3a7bb564b1f2f88
PC <PC@172.20.201.47>
2016-07-27 12:49:00 +0100

    RATING SERVICE DONE!
    COMO MOSTRAR DEPOIS MEDIA RATING?

--- build/ajax/rating.php
+++ build/ajax/rating.php
@@ -3,8 +3,8 @@
 require_once '../db/dbconn.php';
 include_once '../db/session.php';
 include_once '../db/functions.php';
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 $userId = $_SESSION['id'];
 
 if (isset($_POST['rate']) && !empty($_POST['rate'])) {
