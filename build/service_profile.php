52ac4c0b1e44030495ab730e7d926639b476d24e
prcunha <prcunha.383@gmail.com>
2016-07-29 01:14:09 +0100

    SERIVE PROFILE

--- build/service_profile.php
+++ build/service_profile.php
@@ -74,7 +74,7 @@ $serviceId = (filter_var($_GET['Service']));
                         </div>
                         <div class="row">
                             <div class=" col-md-12 text-center" align="center" style="width: auto; height: auto;">
-                                <img class="img-responsive" src="img/faq-2.png" alt="Imagem"style="width: 100%;  padding-bottom: 20px;"/>
+                                <img class="img-responsive" src="img/faq-3.png" alt="Imagem"style="width: 100%;  padding-bottom: 20px;"/>
                             </div>              
                         </div>
                         <footer></footer>
