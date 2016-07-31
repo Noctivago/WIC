67a876a11a12de15890c3a00a9ae6051b3f01a6d
prcunha <prcunha.383@gmail.com>
2016-07-31 01:18:09 +0100

    Edit Profile Service

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -91,7 +91,7 @@ function DB_getStateAsSelectByCountrySelected($pdo, $Country_Id) {
 function DB_GetPicsService($pdo, $serviceId) {
     try {
         $count = 0;
-        $rows = sql($pdo, "Select * from [multimedia] where [Enabled]=1 and [Service_Id]= ? and [First_Page] = 1", array($serviceId), "rows");
+        $rows = sql($pdo, "Select * from [multimedia] where [Enabled]=1 and [Service_Id]= ? ORDER BY [First_Page] DESC", array($serviceId), "rows");
         foreach ($rows as $row) {
             $count+=1;
             echo '<div data-p="144.50"  style="display: none;">
