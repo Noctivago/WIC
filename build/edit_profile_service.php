8f5ba212b73664b88a9799267ccf658ad1bc277f
prcunha <prcunha.383@gmail.com>
2016-07-31 04:09:12 +0100

    Edit Profile Service

--- build/edit_profile_service.php
+++ build/edit_profile_service.php
@@ -317,7 +317,7 @@ ini_set("display_errors", 1);
                                             $city = $_POST['citySelect'];
                                             $msg = DB_UpdateServiceInformation($pdo, $serv, $cname, $cDescription, $cSub, $city);
                                             ?> 
-                                                                                                    <!--<script type="text/javascript">location.href = 'http://wicplanner-testslot.azurewebsites.net/build/service_profile.php?Service=' +<?= $serv ?></script>-->
+                                            <script type="text/javascript">location.href = 'http://wicplanner-testslot.azurewebsites.net/build/service_profile.php?Service=' +<?= $serv ?></script>
                                             <?php
                                         }
 
@@ -382,7 +382,7 @@ ini_set("display_errors", 1);
                     <header class = "box-typical-header-sm">Edit Service </header>
                     <form class = "sign-box" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" id="formm" enctype="multipart/form-data" method="post">
 
-                                                            <!--Change Profile photo:<input id="uploadFile"  accept = "images/*" type="file" name="image" class="img" />-->
+                                                                <!--Change Profile photo:<input id="uploadFile"  accept = "images/*" type="file" name="image" class="img" />-->
                         Change Profile photo:<input id="uploadFile"  accept = "images/*" type="file" name="uploadFile" class="img" />
                         New Files: <input type="file" id="files" name="file[]" accept = "images/*" multiple/><br/>
                         <div id="dvpreview"></div>
