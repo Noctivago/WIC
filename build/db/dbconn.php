35e06ccd2b9bc93afae3ad83fb9bc358d1f23662
prcunha <prcunha.383@gmail.com>
2016-07-31 03:40:34 +0100

    Edit Profile Service

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -3577,6 +3577,7 @@ function DB_DisplyPicuresToRemove($pdo, $serviceId) {
         foreach ($rows as $row) {
             $image = $row['Multimedia_Path'];
             echo '<img src="' . $image . '" style="width:64px;height:64px">';
+            echo '<button type="button">Click Me!</button>';
         }
         echo '</section>';
     } catch (Exception $exc) {
