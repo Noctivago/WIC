5c2c333c3afbb1471a094fd4f0c5a22a0266950f
prcunha <prcunha.383@gmail.com>
2016-08-01 21:53:24 +0100

    Service Profile

--- build/service_profile.php
+++ build/service_profile.php
@@ -458,16 +458,13 @@ $serviceId = (filter_var($_GET['Service']));
 
 <script src = "js/lib/jquery/jquery.min.js" type = "text/javascript"></script>
 <script src="js/lib/tether/tether.min.js" type="text/javascript"></script>
-
 <script src="js/lib/tether/tether.min.js"></script>
 <script src="js/lib/bootstrap/bootstrap.min.js"></script>
 <script src="js/plugins.js"></script>
-
 <script type="text/javascript" src="js/lib/jqueryui/jquery-ui.min.js"></script>
 <script type="text/javascript" src="js/lib/lobipanel/lobipanel.min.js"></script>
 <script type="text/javascript" src="js/lib/match-height/jquery.matchHeight.min.js"></script>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
 <script src="js/lib/salvattore/salvattore.min.js"></script>
 <script src="js/lib/ion-range-slider/ion.rangeSlider.js"></script>
 
@@ -644,7 +641,6 @@ $serviceId = (filter_var($_GET['Service']));
 <script src="js/lib/jquery-tag-editor/jquery.tag-editor.min.js"></script>
 <script src="js/lib/bootstrap-select/bootstrap-select.min.js"></script>
 <script src="js/lib/select2/select2.full.min.js"></script>
-
 <script src="js/app.js"></script>
 
 </body>
