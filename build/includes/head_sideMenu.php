bbb972ccb5001f2be895e10b31566701bd0cef23
prcunha <prcunha.383@gmail.com>
2016-08-03 00:01:46 +0100

    SEARCH

--- build/includes/head_sideMenu.php
+++ build/includes/head_sideMenu.php
@@ -498,30 +498,18 @@ include '../build/db/session.php';
             </ul>
         </nav>
         <script>
-            function UpdateQueryString(url, key, value) {
-                if (!url)
-                    url = window.location.href;
-                var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"), hash;
-                if (re.test(url)) {
-                    if (typeof value !== 'undefined' && value !== null)
-                        return url.replace(re, '$1' + key + "=" + value + '$2$3');
-                    else {
-                        hash = url.split('#');
-                        url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
-                        if (typeof hash[1] !== 'undefined' && hash[1] !== null)
-                            url += '#' + hash[1];
-                        return url;
-                    }
+            function updateQueryStringParameter(uri, key, value) {
+                var re = new RegExp("([?|&])" + key + "=.*?(&|#|$)", "i");
+                if (uri.match(re)) {
+                    return uri.replace(re, '$1' + key + "=" + value + '$2');
                 } else {
-                    if (typeof value !== 'undefined' && value !== null) {
-                        var separator = url.indexOf('?') !== -1 ? '&' : '?';
-                        hash = url.split('#');
-                        url = hash[0] + separator + key + '=' + value;
-                        if (typeof hash[1] !== 'undefined' && hash[1] !== null)
-                            url += '#' + hash[1];
-                        return url;
-                    } else
-                        return url;
+                    var hash = '';
+                    if (uri.indexOf('#') !== -1) {
+                        hash = uri.replace(/.*#/, '#');
+                        uri = uri.replace(/#.*/, '');
+                    }
+                    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+                    return uri + separator + key + "=" + value + hash;
                 }
             }
             function sendInvite() {
