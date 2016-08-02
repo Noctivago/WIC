b3c7f251915c280ead6437f4a0fed2b3c8369d9d
PC <PC@172.20.201.47>
2016-08-02 18:13:48 +0100

    SEARCH

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -3680,8 +3680,9 @@ function DB_getServicesForIndexByQuery($pdo, $CategoryId, $name, $city, $SubCate
         AND [Multimedia].[First_Page] =  1
         AND [Service].[Name] Like '%" . $name . "%'
         AND [Sub_Category].[Category_Id] Like '%" . $CategoryId . "'
-        AND [City].[Id] Like '" . $city . "%' 
+        AND [City].[Id] = " . $city . "
         AND [Sub_Category].[Id] Like '%" . $SubCategory . "'", array(), "rows");
+        //AND [City].[Id] Like '" . $city . "%' 
         foreach ($rows as $row) {
             echo '<div class = "card-grid-col">
             <article class = "card-typical">
