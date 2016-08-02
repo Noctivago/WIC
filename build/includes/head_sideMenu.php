af865991490163d0bbac052b1f4e64960b4c7bd7
prcunha <prcunha.383@gmail.com>
2016-08-02 22:49:48 +0100

    SEARCH

--- build/includes/head_sideMenu.php
+++ build/includes/head_sideMenu.php
@@ -287,12 +287,7 @@ include '../build/db/session.php';
 
                 <header class="side-menu-title">Start Planning</header>
                 <li class="brown with-sub">
-                    <!--preg_replace("#&d=.*&#", '&d=newvalue&', $_SERVER['REQUEST_URI'])-->
-                    <?php
-                    $oldurl = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-                    ?>
-
-                    <a class="lbl" href="<?= $selfUrl = preg_replace("/Category=\/", "Category=1", $oldurl); ?>"><i class="fa fa-bank"></i> Space</a>
+                    <a class="lbl" href="<?= $selfUrl . '&Category=1' ?>"><i class="fa fa-bank"></i> Space</a>
                 </li>
                 <li class="brown with-sub">
                     <a class="lbl" href="<?= $selfUrl . '&Category=2' ?>"><i class="fa fa-cutlery"></i> Food</a>
