4dfd2969d761272036deeccfe8266b1e4ec40771
PC <PC@172.20.201.47>
2016-08-02 17:15:32 +0100

    Public Profile

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<meta http-equiv="refresh" content="0;URL=/public/index.php" />
+<?php
+if (isset($_SESSION['id'])) {
+    if ($_SESSION['role'] === 'organization') {
+        header("location: /build/profile_org.php");
+    }
+    if ($_SESSION['role'] === 'user') {
+        header("location: /build/index.php");
+    }
+} else {
+    header("location: /public/index.php");
+}
+?>
