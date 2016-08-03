d5e02369c69d75621fb64573e5a8db4aefa7f018
prcunha <prcunha.383@gmail.com>
2016-08-03 21:18:55 +0100

    PUBLIC INDEX

--- build/indexPub.php
+++ build/indexPub.php
@@ -188,7 +188,7 @@ include ("./db/dbconn.php");
     <script src="js/lib/tether/tether.min.js"></script>
     <script src="js/lib/bootstrap/bootstrap.min.js"></script>
     <script src="js/plugins.js"></script>
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+
     <script type="text/javascript" src="js/lib/jqueryui/jquery-ui.min.js"></script>
     <script type="text/javascript" src="js/lib/lobipanel/lobipanel.min.js"></script>
     <script type="text/javascript" src="js/lib/match-height/jquery.matchHeight.min.js"></script>
@@ -330,6 +330,7 @@ include ("./db/dbconn.php");
     </script>
 
     <script src="js/app.js"></script>
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
     <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="js/lib/jquery-tag-editor/jquery.caret.min.js"></script>
