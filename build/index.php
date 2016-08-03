d1d90c44591a748db3d505425899d3d046e531aa
PC <PC@172.20.201.47>
2016-08-03 10:29:23 +0100

    SEARCH

--- build/index.php
+++ build/index.php
@@ -42,7 +42,7 @@ include ("./db/dbconn.php");
         } else {
             //echo 'updateQueryStringParameter(PageNum, 1);';
             //$func = "updateQueryStringParameter('PageNum', '1')";
-            echo '<script> updateQueryStringParameter("PageNum","1"); </script>';
+            echo '<script>updateQueryStringParameter("PageNum","1"); </script>';
             //echo '<Script language="text/javascript">' . $func . '</Script>';
             $PageNum = 1;
         }
