d02570f6272b92e1f5ceb0727c33d63f713f73a0
PC <PC@172.20.201.47>
2016-08-03 16:41:22 +0100

    SEARCH

--- build/sign_in.php
+++ build/sign_in.php
@@ -8,10 +8,12 @@ session_start();
 
 if (isset($_SESSION['id'])) {
     if ($_SESSION['role'] === 'organization') {
-         header("location: ../build/profile_org.php");
+        //header("location: ../build/profile_org.php");
+        header("location: http://" . $_SERVER['HTTP_HOST'] . "/build/profile_org.php");
     }
     if ($_SESSION['role'] === 'user') {
-        header("location: ../build/index.php");
+        //header("location: ../build/index.php");
+        header("location: http://" . $_SERVER['HTTP_HOST'] . "/build/index.php");
     }
 }
 ?>
