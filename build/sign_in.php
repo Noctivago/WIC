73a0463b0032135342ca8da2bd27f957bd2c9e8f
PC <PC@172.20.201.47>
2016-08-03 16:46:25 +0100

    SEARCH

--- build/sign_in.php
+++ build/sign_in.php
@@ -9,7 +9,10 @@ session_start();
 if (isset($_SESSION['id'])) {
     if ($_SESSION['role'] === 'organization') {
         //header("location: ../build/profile_org.php");
-        header("location: http://" . $_SERVER['HTTP_HOST'] . "/build/profile_org.php");
+        $use = $_SESSION['id'];
+        $idOg = DB_GetOrgIdByUserBossId2($pdo, $use);
+        $idorga = $idOg['Id'];
+        header("location: http://" . $_SERVER['HTTP_HOST'] . "/build/profile_org.php?Organization=$idorga");
     }
     if ($_SESSION['role'] === 'user') {
         //header("location: ../build/index.php");
