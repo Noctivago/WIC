b813d53986a92359805c69021d3afa6caf9759f0
prcunha <prcunha.383@gmail.com>
2016-08-03 21:27:27 +0100

    PUBLIC INDEX

--- build/includes/head_sideMenuPub.php
+++ build/includes/head_sideMenuPub.php
@@ -40,8 +40,8 @@ if (isset($_SESSION['id'])) {
         <link href="css/main.css" rel="stylesheet" type="text/css"/>
 
         <!--<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">-->
-        <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+        <!--<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>-->
+        <!--<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>-->
 
     </head>
     <body class="with-side-menu control-panel control-panel-compact">
