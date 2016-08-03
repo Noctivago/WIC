49b7d554099bd335de28eb542cdbd2c244a697c7
prcunha <prcunha.383@gmail.com>
2016-08-03 20:57:42 +0100

    PUBLIC INDEX

--- build/includes/head_sideMenuPub.php
+++ build/includes/head_sideMenuPub.php
@@ -39,9 +39,9 @@ if (isset($_SESSION['id'])) {
         <link href="css/lib/font-awesome/font-awesome.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/main.css" rel="stylesheet" type="text/css"/>
 
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<!--        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
         <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>-->
         <script>
             function openWindow() {
                 $('#myPopup').popup("open");
