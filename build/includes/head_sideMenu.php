81e134b54e85d6f8164b6db357427414f79c5af3
PC <PC@172.20.201.47>
2016-08-03 12:42:59 +0100

    SEARCH

--- build/includes/head_sideMenu.php
+++ build/includes/head_sideMenu.php
@@ -301,7 +301,7 @@ include '../build/db/session.php';
                 <header class="side-menu-title">Start Planning</header>
                 <!--updateQueryStringParameter(uri, key, value)-->  
                 <li class="brown with-sub">
-                    <a class="lbl" onclick="updateQueryStringParameter('Category', '1');"><i class="fa fa-bank"></i> Space</a>
+                    <a class="lbl" onclick="updateQueryStringParameter('Category', '1');setPage('0');"><i class="fa fa-bank"></i> Space</a>
                 </li>
                 <li class="brown with-sub">
                     <a class="lbl" onclick="updateQueryStringParameter('Category', '2');"><i class="fa fa-cutlery"></i> Food</a>
