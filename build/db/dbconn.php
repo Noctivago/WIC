41d3f2b617e4b8056abaecae9b445a01ed850d61
prcunha <prcunha.383@gmail.com>
2016-08-03 02:36:08 +0100

    SEARCH

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -3844,33 +3844,34 @@ function DB_CountServices($pdo, $CategoryId, $name, $city, $SubCategory) {
         AND [Sub_Category].[Category_Id] Like '%" . $CategoryId . "'
         AND [City].[Name] Like '%" . $city . "'
         AND [Sub_Category].[Id] Like '%" . $SubCategory . "'", array(), "rows");
-        echo ' <div style="padding-left: 500px;">
-                <nav>
-                <ul class="pagination">
-                    <li class="page-item disabled">
-                        <a class="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    </li>';
+        echo '<div style="padding-left: 500px;">
+                    <nav>
+                        <ul class="pagination">
+                            <li class="page-item disabled">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                            </li>';
         foreach ($rows as $row) {
             $pageNum = ($row['COUNTER'] / 50);
             for ($i = 0; $i <= $pageNum; $i++) {
 //                    <li class="page-item active">
 //                        <a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
 //                    </li>
+                //<li class="page-item"><a class="page-link" href="#">2</a></li>
                 echo '<li class="page-item"><a class="page-link">' . $i + 1 . '</a></li>';
             }
         }
         echo '<li class="page-item">
-                        <a class="page-link" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>';
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>';
 
         //<a href="service_profile.php?service=' . $row['SID'] . '" class="card-typical-likes">
         //<i class="font-icon font-icon-eye">' . DB_GetNumberServiceViews($pdo, $row['SID']) . '</i> 
