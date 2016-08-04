bdf42e6c93dad0f23ce41fbcd72f2b55d739fcea
prcunha <prcunha.383@gmail.com>
2016-08-04 01:09:12 +0100

    PUBLIC INDEX

--- build/includes/head_sideMenuPub.php
+++ build/includes/head_sideMenuPub.php
@@ -68,7 +68,7 @@ if (isset($_SESSION['id'])) {
                                 </a>
                             </div>
                             <div class="dropdown user-menu">
-                                <a href="../build/sign_in.php?redUrl=/build/index.php">
+                                <a href="../build/sign_in.php?redUrl=/build/index.php"
                                     <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown"  aria-haspopup="true"  aria-expanded="false">
                                         <img src="img/avatar-2-64.png" alt="">
                                     </button>
