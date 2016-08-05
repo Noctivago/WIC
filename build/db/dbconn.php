8ccfab82032f744ec69f1d2505c6a873036bbef2
prcunha <prcunha.383@gmail.com>
2016-08-06 00:11:24 +0100

    SHARE BUTTONS

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -4054,7 +4054,7 @@ function DB_TwitterShareFunc($pdo, $serviceId) {
         foreach ($rows as $row) {
 
             //echo '<a class="twitter-share-button"
-            echo '<a class="font-icon font-icon-twitter
+            echo '<a class="font-icon font-icon-twitter"
                 href="https://twitter.com/share"
                 data-size="large"
                 data-url="' . $_SERVER['PHP_SELF'] . $_SERVER['REQUEST_URI'] . '"
