bf3d0b6a73aa118dca36821fa48d3ae5f957136f
prcunha <prcunha.383@gmail.com>
2016-08-06 09:38:28 +0100

    WIC PLANNER FIX

--- build/my_wicplanner.php
+++ build/my_wicplanner.php
@@ -142,8 +142,8 @@ $msg = '';
         var wicDate = document.getElementById("daterange3").value;
         var wId = wicId;
         //ESTA A FALHAR AQUI!! OU NA VALIDAÇÂO OU ENTAO NO GET VALUE
-        if (document.getElementById("name") !== null) {
-            alert('!NULL > TTF #>' + document.getElementById("name").value + ' < + DATE > ' + document.getElementById("daterange3").value);
+        if (document.getElementById("Xname") !== null) {
+            alert('!NULL > TTF #>' + document.getElementById("Xname").value + ' < + DATE > ' + document.getElementById("daterange3").value);
         }
         if (wicName !== "") {
             $.ajax({
