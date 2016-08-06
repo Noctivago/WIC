e02865e7d4fd96349caf83bb441454c7a4236fcc
prcunha <prcunha.383@gmail.com>
2016-08-06 01:59:09 +0100

    SHARE BUTTONS

--- build/sign_in.php
+++ build/sign_in.php
@@ -133,7 +133,7 @@ if (isset($_SESSION['id'])) {
                     </div>
                     <p class="sign-note">  <?= $msg; ?> </p>
                     <button type="submit" name="signin" class="btn btn-rounded">Sign in</button>
-                    <p class="sign-note">New to our website? <a href="sign_up_user.php">Sign up as an user</a><a href="sign_up_org.php"> Sign up as an Organization</a></p>
+                    <p class="sign-note">New to our website? <a href="sign_up_user.php">Sign up as a planner</a><a href="sign_up_org.php"> Sign up as a vendor</a></p>
                     <!--<button type="button" class="close">
                         <span aria-hidden="true">&times;</span>
                     </button>-->
