3bbc54cf1eb721c5c4c91ed0f435d33e41116986
prcunha <prcunha.383@gmail.com>
2016-08-09 20:37:57 +0100

    HEAD SIDE MENU SELECT

--- build/db/dbconn.php
+++ build/db/dbconn.php
@@ -3970,7 +3970,9 @@ function DB_getServicesForPublicIndexByQuery($pdo, $CategoryId, $name, $city, $S
             </div>
             <div class = "card-typical-section card-typical-content">
             <div class = "photo">
+            <a href="service_profile.php?Service=' . $row['SID'] . '">
             <img src = "' . $row['MPP'] . '" alt = "Service Pic" max-height = "185" max-width = "110">
+            </a>
             </div>
             <header class = "title"><a href = "../build/sign_in.php?redUrl=/build/service_profile.php?Service=' . $row['SID'] . '">' . $row['SNA'] . '</a></header>
             <p style="overflow:hidden; max-height:75px; ">' . $row['SDE'] . '</p>
