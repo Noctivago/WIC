4a9185a4c812819502a03b3d2c5a3a7c393eb1c9
prcunha <prcunha.383@gmail.com>
2016-08-09 22:00:15 +0100

    HEAD SIDE MENU SELECT

--- build/ajax/test.php
+++ build/ajax/test.php
@@ -21,13 +21,12 @@ include_once '../db/session.php';
 //echo $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 echo DB_getServicesForIndexCount($pdo, $CategoryId, $name, $city, $SubCategory);
 
-$msg = '<head></head>';
-$msg .= '<body>';
-$msg .= '<p>Hello!</p><br>';
-$msg .= '<p>Welcome to Wic the biggest community of events and entertainment on earth.</p><br>';
-$msg .= '<p>Talk, deal and start planning the event of your dreams.</p><br>';
-$msg .= '<p><a href="http://google.com>LINK</a></p><br>';
-$msg .= '</body>';
+$msg .= 'Hello!<br>';
+$msg .= 'Welcome to Wic the biggest community of events and entertainment on earth.<br>';
+$msg .= 'Talk, deal and start planning the event of your dreams.<br>';
+$msg .= 'http://google.com<br>';
+$msg .= '<img src="http://www.w3schools.com/tags/smiley.gif" alt="Smiley face" width="42" height="42"><br>';
+
 $email = 'prcunha.383@gmail.com';
 $code = 'qeqwedwqdqwe';
 $to = 'prcunha.383@gmail.com';
