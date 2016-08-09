716d9655360254822c19d084ce5cd80a8091503d
prcunha <prcunha.383@gmail.com>
2016-08-09 22:26:55 +0100

    HEAD SIDE MENU SELECT

--- build/ajax/test.php
+++ build/ajax/test.php
@@ -21,34 +21,14 @@ include_once '../db/session.php';
 //echo $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 echo DB_getServicesForIndexCount($pdo, $CategoryId, $name, $city, $SubCategory);
 
-$msg .= 'Hello!<br>';
-$msg .= 'Welcome to Wic the biggest community of events and entertainment on earth.<br>';
-$msg .= 'Talk, deal and start planning the event of your dreams.<br>';
-$msg .= 'http://google.com<br>';
-$msg .= '<img src="http://www.w3schools.com/tags/smiley.gif" alt="Smiley face" width="42" height="42"><br>';
-
-$email = 'prcunha.383@gmail.com';
-$code = 'qeqwedwqdqwe';
-$to = 'prcunha.383@gmail.com';
-$subject = 'TESTE';
-return sendEmail($to, $subject, $msg);
-//$msg = "ACCOUNT INFORMATION IS BEING SENT! PLEASE WAIT!";
+//$msg .= 'Hello!<br>';
+//$msg .= 'Welcome to Wic the biggest community of events and entertainment on earth.<br>';
+//$msg .= 'Talk, deal and start planning the event of your dreams.<br>';
+//$msg .= 'http://google.com<br>';
+//$msg .= '<img src="http://www.w3schools.com/tags/smiley.gif" alt="Smiley face" width="42" height="42"><br>';
+//
 //$email = 'prcunha.383@gmail.com';
 //$code = 'qeqwedwqdqwe';
 //$to = 'prcunha.383@gmail.com';
-//$link = 'http://' . $_SERVER['HTTP_HOST'] . '/build/account_confirmation_link.php?EM=' . $email . '&AC=' . $code . '';
-//$subject = "Welcome to the biggest community of events";
-//$body = "Hello " . $name . ", <br><br>"
-//        . "Welcome to Wic the biggest community of events and entertainment on earth.<br>"
-//        . "Talk, deal and start planning the event of your dreams.<br>"
-//        . "Click on the following link to validate your account: <a href=\"" . $link . ">Click to validate your account</a><br><br>"
-//        . "Event your life ! You Can Event ! <br><br>"
-//        . "Note: Please do not reply to this email! Thanks!";
-//return sendEmail($to, $subject, $body);
-
-
-//echo '<br>http://' . $_SERVER['HTTP_HOST'];
-//echo '<br>';
-//$EM = "paulo.cunha@esprominho.com";
-//$AC = '14551ASDF';
-//echo 'http://' . $_SERVER['HTTP_HOST'] . '/build/account_confirmation_link.php?EM=' . $EM . '&AC=' . $AC . '';
+//$subject = 'TESTE';
+//return sendEmail($to, $subject, $msg);
