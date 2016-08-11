928104627c66b5ff2a6b1393fdfd048a2789033a
prcunha <prcunha.383@gmail.com>
2016-08-11 18:40:16 +0100

    FOLLOW US FB

--- build/ajax/test.php
+++ build/ajax/test.php
@@ -21,6 +21,29 @@ include_once '../db/session.php';
 //echo $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 echo DB_getServicesForIndexCount($pdo, $CategoryId, $name, $city, $SubCategory);
 
+
+//Set your App ID and App Secret.
+$appID = '231111773916943';
+$appSecret = 'c7d95d5f1f2f0210462240a0d9c9bf0e';
+ 
+//Create an access token using the APP ID and APP Secret.
+$accessToken = $appID . '|' . $appSecret;
+ 
+//The ID of the Facebook page in question.
+$id = '231111773916943';
+ 
+//Tie it all together to construct the URL
+$url = "https://graph.facebook.com/$id/posts?access_token=$accessToken";
+ 
+//Make the API call
+$result = file_get_contents($url);
+ 
+//Decode the JSON result.
+$decoded = json_decode($result, true);
+ 
+//Dump it out onto the page so that we can take a look at the structure of the data.
+var_dump($decoded);
+
 //$msg .= 'Hello!<br>';
 //$msg .= 'Welcome to Wic the biggest community of events and entertainment on earth.<br>';
 //$msg .= 'Talk, deal and start planning the event of your dreams.<br>';
